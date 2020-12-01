cf612b227a01da5e391f87115e3b28b63f81ce34
pysaquib <iamsaquibnasim@gmail.com>
2020-12-01 11:56:23 +0530

    deploy script addded

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+git branch -f gh-pages
+
+git checkout gh-pages
+
+git reset --hard origin/master
+
+npm run build
+
+cp -r build/* .
+
+echo 'merakilearn.org' > CNAME
+
+git add -A .
+
+git commit -a -m 'gh-pages update'
+
+git push origin gh-pages --force
+
+git checkout master
