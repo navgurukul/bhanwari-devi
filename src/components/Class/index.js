75d3eba18276a718249c26631500f801f067e6be
Tarun Dugar <tarundugar1992@gmail.com>
2021-05-01 18:43:01 +0530

    reusable form component

--- src/components/Class/index.js
+++ src/components/Class/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import moment from "moment";
 import axios from "axios";
@@ -6,16 +6,36 @@ import axios from "axios";
 import { TIME_CONSTANT } from "./constant";
 import { actions } from "./redux/action";
 import Loader from "../common/Loader";
+import Form from "../common/form";
 import { METHODS } from "../../services/api";
-
 import "./styles.scss";
+
+const TITLE = "title";
+const DESCRIPTION = "description";
+const FACILITATOR_NAME = "facilitator_name";
+const FACILITATOR_EMAIL = "facilitator_email";
+const START_TIME = "start_time";
+const CLASS_START_TIME = "class_start_time";
+const CLASS_END_TIME = "class_end_time";
+const LANG = "lang";
+const TYPE = "type";
+const COURSE_ID = "course_id";
 
 function Class() {
   const dispatch = useDispatch();
+  const initialFormState = useRef({
+    [TITLE]: "",
+    [DESCRIPTION]: "",
+    [FACILITATOR_NAME]: "",
+    [FACILITATOR_EMAIL]: "",
+    [START_TIME]: moment().format("YYYY-MM-DD"),
+    [CLASS_START_TIME]: "",
+    [CLASS_END_TIME]: "",
+    [LANG]: "hi",
+    [TYPE]: "doubt_class",
+    [COURSE_ID]: "",
+  });
   const user = useSelector(({ User }) => User);
-  const [classDate, onClassDateChange] = useState(
-    moment().format("YYYY-MM-DD")
-  );
   const { loading } = useSelector(({ Class }) => Class);
   const rolesList = user.data.user.rolesList;
 
@@ -83,123 +103,169 @@ function Class() {
   return (
     <div className="ng-create-class">
       <h2 className="title">Create A Class </h2>
-      <form className="form" onSubmit={onFormSubmit}>
-        <label htmlFor="title">Title</label>
-        <input
-          className="input-field"
-          type="text"
-          name="title"
-          id="title"
-          required
-          aria-required
-        />
-        <label htmlFor="description">Description</label>
-        <textarea
-          name="description"
-          rows="10"
-          id="description"
-          className="textarea-field"
-          required
-          aria-required
-        />
-        {canSpecifyFacilitator && (
-          <>
-            <label htmlFor="facilitator_name">Facilitator Name</label>
-            <input
-              className="input-field"
-              type="text"
-              name="facilitator_name"
-              id="facilitator_name"
-              required
-              aria-required
-            />
-            <label htmlFor="facilitator_email">Facilitator Email</label>
-            <input
-              className="input-field"
-              type="email"
-              name="facilitator_email"
-              id="facilitator_email"
-              required
-              aria-required
-            />
-          </>
-        )}
-        <label htmlFor="start_time">Date</label>
-        <input
-          className="input-field input-field--short"
-          type="date"
-          name="start_time"
-          value={classDate}
-          onChange={(e) => {
-            onClassDateChange(e.target.value);
-          }}
-          id="start_time"
-          required
-          aria-required
-        />
-        <label htmlFor="class_start_time">Start Time</label>
-        <input
-          className="input-field input-field--short"
-          type="time"
-          name="class_start_time"
-          id="class_start_time"
-          required
-          aria-required
-        />
-        <label htmlFor="class_end_time">End Time</label>
-        <input
-          className="input-field input-field--short"
-          type="time"
-          name="class_end_time"
-          id="class_end_time"
-          required
-          aria-required
-        />
-        <label htmlFor="lang">Select Language</label>
-        <select
-          className="create-class-select"
-          name="lang"
-          id="name"
-          required
-          aria-required
-        >
-          <option value="en">English</option>
-          <option value="hi" selected>
-            Hindi
-          </option>
-          <option value="te">Telugu</option>
-          <option value="ta">Tamil</option>
-        </select>
-        <label htmlFor="type">Select Class Type</label>
-        <select
-          className="create-class-select"
-          name="type"
-          id="type"
-          required
-          aria-required
-        >
-          <option value="workshop">Workshop</option>
-          <option value="doubt_class" selected>
-            Doubt Class
-          </option>
-        </select>
-        <label htmlFor="course_id">Select Course</label>
-        <select className="create-class-select" name="course_id" id="course_id">
-          <option value="" disabled selected>
-            Select a course from options below
-          </option>
-          {allCourse.map((item, index) => {
-            return (
-              <option key={index} value={item.id}>
-                {item.name}
-              </option>
-            );
-          })}
-        </select>
-        <button type="submit" className="submit" disabled={loading}>
-          {loading ? <Loader /> : "CREATE CLASS"}
-        </button>
-      </form>
+      <Form
+        className="form"
+        onSubmit={onFormSubmit}
+        initialFieldsState={initialFormState.current}
+      >
+        {({ formFieldsState, setFormField }) => {
+          return (
+            <>
+              <label htmlFor="title">Title</label>
+              <input
+                className="input-field"
+                type="text"
+                name={TITLE}
+                onChange={(e) => {
+                  setFormField(e.target.value, TITLE);
+                }}
+                value={formFieldsState[TITLE]}
+                id="title"
+                required
+                aria-required
+              />
+              <label htmlFor="description">Description</label>
+              <textarea
+                name={DESCRIPTION}
+                rows="10"
+                id="description"
+                onChange={(e) => {
+                  setFormField(e.target.value, DESCRIPTION);
+                }}
+                value={formFieldsState[DESCRIPTION]}
+                className="textarea-field"
+                required
+                aria-required
+              />
+              {canSpecifyFacilitator && (
+                <>
+                  <label htmlFor="facilitator_name">Facilitator Name</label>
+                  <input
+                    className="input-field"
+                    type="text"
+                    name={FACILITATOR_NAME}
+                    value={formFieldsState[FACILITATOR_NAME]}
+                    onChange={(e) => {
+                      setFormField(e.target.value, FACILITATOR_NAME);
+                    }}
+                    id="facilitator_name"
+                    required
+                    aria-required
+                  />
+                  <label htmlFor="facilitator_email">Facilitator Email</label>
+                  <input
+                    className="input-field"
+                    type="email"
+                    value={formFieldsState[FACILITATOR_EMAIL]}
+                    onChange={(e) => {
+                      setFormField(e.target.value, FACILITATOR_EMAIL);
+                    }}
+                    name={FACILITATOR_EMAIL}
+                    id="facilitator_email"
+                    required
+                    aria-required
+                  />
+                </>
+              )}
+              <label htmlFor="start_time">Date</label>
+              <input
+                className="input-field input-field--short"
+                type="date"
+                name={START_TIME}
+                value={formFieldsState[START_TIME]}
+                onChange={(e) => {
+                  setFormField(e.target.value, START_TIME);
+                }}
+                id="start_time"
+                required
+                aria-required
+              />
+              <label htmlFor="class_start_time">Start Time</label>
+              <input
+                className="input-field input-field--short"
+                type="time"
+                name={CLASS_START_TIME}
+                onChange={(e) => {
+                  setFormField(e.target.value, CLASS_START_TIME);
+                }}
+                value={formFieldsState[CLASS_START_TIME]}
+                id="class_start_time"
+                required
+                aria-required
+              />
+              <label htmlFor="class_end_time">End Time</label>
+              <input
+                className="input-field input-field--short"
+                type="time"
+                name={CLASS_END_TIME}
+                onChange={(e) => {
+                  setFormField(e.target.value, CLASS_END_TIME);
+                }}
+                value={formFieldsState[CLASS_END_TIME]}
+                id="class_end_time"
+                required
+                aria-required
+              />
+              <label htmlFor="lang">Select Language</label>
+              <select
+                className="create-class-select"
+                name={LANG}
+                value={formFieldsState[LANG]}
+                onChange={(e) => {
+                  setFormField(e.target.value, LANG);
+                }}
+                id="lang"
+                required
+                aria-required
+              >
+                <option value="en">English</option>
+                <option value="hi">Hindi</option>
+                <option value="te">Telugu</option>
+                <option value="ta">Tamil</option>
+              </select>
+              <label htmlFor="type">Select Class Type</label>
+              <select
+                className="create-class-select"
+                name={TYPE}
+                value={formFieldsState[TYPE]}
+                onChange={(e) => {
+                  setFormField(e.target.value, TYPE);
+                }}
+                id="type"
+                required
+                aria-required
+              >
+                <option value="workshop">Workshop</option>
+                <option value="doubt_class">Doubt Class</option>
+              </select>
+              <label htmlFor="course_id">Select Course</label>
+              <select
+                className="create-class-select"
+                name={COURSE_ID}
+                value={formFieldsState[COURSE_ID]}
+                onChange={(e) => {
+                  setFormField(e.target.value, COURSE_ID);
+                }}
+                id="course_id"
+              >
+                <option value="" disabled>
+                  Select a course from options below
+                </option>
+                {allCourse.map((item, index) => {
+                  return (
+                    <option key={index} value={item.id}>
+                      {item.name}
+                    </option>
+                  );
+                })}
+              </select>
+              <button type="submit" className="submit" disabled={loading}>
+                {loading ? <Loader /> : "CREATE CLASS"}
+              </button>
+            </>
+          );
+        }}
+      </Form>
     </div>
   );
 }
