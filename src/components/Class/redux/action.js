34a7b90788c3968197dbb480953e03159cce28a7
komalng <komal@navgurukul.org>
2021-05-14 12:01:35 +0530

    Remove the redux code that has the create class api.

--- src/components/Class/redux/action.js
+++ src/components/Class/redux/action.js
@@ -2,9 +2,6 @@ export const types = {
   GET_CLASSES_INTENT: "GET_CLASSES_INTENT",
   GET_CLASSES_INTENT_RESOLVED: "GET_CLASSES_INTENT_RESOLVED",
   GET_CLASSES_INTENT_REJECTED: "GET_CLASSES_INTENT_REJECTED",
-  GET_CREATE_CLASS_INTENT: "GET_CREATE_CLASS_INTENT",
-  GET_CREATE_CLASS_INTENT_RESOLVED: "GET_CREATE_CLASS_INTENT_RESOLVED",
-  GET_CREATE_CLASS_INTENT_REJECTED: "GET_CREATE_CLASS_INTENT_REJECTED",
 };
 
 export const actions = {
@@ -23,24 +20,6 @@ export const actions = {
   getClassesRejected(error) {
     return {
       type: types.GET_CLASSES_INTENT_REJECTED,
-      error,
-    };
-  },
-  createClass(data) {
-    return {
-      type: types.GET_CREATE_CLASS_INTENT,
-      data,
-    };
-  },
-  createClassResolved(data) {
-    return {
-      type: types.GET_CREATE_CLASS_INTENT_RESOLVED,
-      data,
-    };
-  },
-  createClassRejected(error) {
-    return {
-      type: types.GET_CREATE_CLASS_INTENT_REJECTED,
       error,
     };
   },
