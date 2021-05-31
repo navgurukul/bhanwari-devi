e7159fb5db71b43b8bd56a5a4544e6192d292292
komalahire <komala@navgurukul.org>
2021-05-31 23:01:49 +0530

    Footer is shown on only the home page, change bg color,

--- src/pages/Course/index.js
+++ src/pages/Course/index.js
@@ -2,14 +2,18 @@ import React from "react";
 
 import CourseComponent from "../../components/Course";
 import ContinueExercise from "../../components/Course/ContinueExercise";
+import Footer from "../../components/Footer";
 import "./styles.scss";
 
 function Course() {
   return (
-    <div className="ng-course-container">
-      <h2>Hello, There</h2>
-      <ContinueExercise />
-      <CourseComponent />
+    <div className="layout">
+      <div className="ng-course-container">
+        <h2>Hello, There</h2>
+        <ContinueExercise />
+        <CourseComponent />
+      </div>
+      <Footer />
     </div>
   );
 }
