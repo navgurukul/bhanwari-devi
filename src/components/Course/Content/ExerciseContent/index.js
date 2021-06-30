5c5574c64cc2caeacde10f00504e5461af05eb46
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2021-06-30 21:04:27 +0530

    Video, Image & Table are visiblw now

--- src/components/Course/Content/ExerciseContent/index.js
+++ src/components/Course/Content/ExerciseContent/index.js
@@ -3,9 +3,11 @@ import PropTypes from "prop-types";
 import ReactMarkdown from "react-markdown";
 import htmlParser from "react-markdown/plugins/html-parser";
 import YouTube from "react-youtube";
+// import { useTable } from "react-table";
 import get from "lodash/get";
 
 import "./styles.scss";
+import ExerciseContentTable from "../ExerciseContentTable/ExerciseContentTable";
 
 // See https://github.com/aknuds1/html-to-react#with-custom-processing-instructions
 // for more info on the processing instructions
@@ -16,8 +18,99 @@ const parseHtml = htmlParser({
   ],
 });
 
+// const table = (data) => {
+//   const tableInstance = useTable({
+//     columns: data.value.header,
+//     data: data.value.value
+//   })
+
+//   const {
+//     getTableProps,
+//     getTableBodyProps,
+//     headerGroups,
+//     rows,
+//     prepareRow,
+//   } = tableInstance
+
+//   return (
+//     <table {...getTableProps()}>
+//       <thead>
+//         {
+//         headerGroups.map(headerGroup => (
+//           <tr {...headerGroup.getHeaderGroupProps()}>
+//             {
+//             headerGroup.headers.map(column => (
+//               <th {...column.getHeaderProps()}>
+//                 {
+//                 column.render('Header')}
+//               </th>
+//             ))}
+//           </tr>
+//         ))}
+//       </thead>
+//       <tbody {...getTableBodyProps()}>
+//         {
+//         rows.map(row => {
+//           prepareRow(row)
+//           return (
+//             <tr {...row.getRowProps()}>
+//               {
+//               row.cells.map(cell => {
+//                 return (
+//                   <td {...cell.getCellProps()}>
+//                     {
+//                     cell.render('Cell')}
+//                   </td>
+//                 )
+//               })}
+//             </tr>
+//           )
+//         })}
+//       </tbody>
+//     </table>
+//   )
+// }
+
 const RenderContent = ({ data }) => {
+  if (data.type === "image") {
+    console.log("get(data)", get(data));
+    return <img className="image" src={get(data, "value.url")} alt="content" />;
+  }
+  if (data.type === "youtube") {
+    console.log("Video aa rha");
+    return <YouTube className={"youtube-video"} videoId={data.value} />;
+  }
+  if (data.type === "table") {
+    const columns = data.value.header;
+    const tableData = data.value.value;
+
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            {columns.map((col) => {
+              return <th>{col}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((rows) => {
+            return (
+              <tr>
+                {rows.map((row) => {
+                  return <th>{row}</th>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+
+    // return <ExerciseContentTable data={data}/>
+  }
   if (data.type === "markdown") {
+    console.log("ho rha h");
     return (
       <ReactMarkdown
         source={data.value}
@@ -25,7 +118,8 @@ const RenderContent = ({ data }) => {
         astPlugins={[parseHtml]}
       />
     );
-  } else if (data.type === "python" || "javascript") {
+  }
+  if (data.type === "python" || "javascript") {
     return (
       <code className="language-python code-block">
         {" "}
@@ -33,28 +127,33 @@ const RenderContent = ({ data }) => {
         {get(data, "value.code")} <br />
       </code>
     );
-  } else if (data.type === "bash") {
+  }
+  if (data.type === "bash") {
     return (
       <code className="language-bash code-block">
         {" "}
         {get(data, "value.code")}{" "}
       </code>
     );
-  } else if (data.type === "image") {
-    return <img className="image" src={get(data, "value.url")} alt="content" />;
-  } else if (data.type === "video") {
-    return <YouTube className={"youtube-video"} videoId={data.value} />;
-  } else if (data.type === "youtube") {
-    return <YouTube className={"youtube-video"} videoId={data.value} />;
-  } else if (data.type === "image") {
-    return <img className="image" src={get(data, "value.url")} alt="content" />;
   }
+  // } else if (data.type == "image") {
+  //   return <img className="image" src={get(data, "value.url")} alt="content" />;
+  // } else if (data.type == "video") {
+  //   return <YouTube className={"youtube-video"} videoId={data.value} />;
+  // }
+  // if (data.type == "youtube") {
+  //   return <YouTube className={"youtube-video"} videoId={data.value} />;
+  // }
+  // if (data.type == "image") {
+  //   console.log("get(data)", get(data))
+  //   return <img className="image" src={get(data, "value.url")} alt="content" />;
+  // }
   return "";
 };
 
 function ExerciseContent(props) {
   const { content = [] } = props;
-  // console.log(content,'content')
+  // console.log('content', content)
 
   if (!content) {
     return "";
