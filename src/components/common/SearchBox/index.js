5b245dd6587a4c9ddb9ab55d703add3922e5d24c
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2021-09-10 23:46:22 +0530

    calendar-consent

--- src/components/common/SearchBox/index.js
+++ src/components/common/SearchBox/index.js
@@ -8,46 +8,6 @@ import { PATHS } from "../../../constant";
 import "./styles.scss";
 
 function SearchBox(props) {
-  // const [isAuthenticated, setIsAuthenticated] = useState(false);
-  // // const [toggle, setToggle] = useState(false);
-  // const user = useSelector(({ User }) => User);
-  // const url = window.location.href;
-  // let code;
-
-  // const userCalender = (code) => {
-  //   return axios({
-  //     method: METHODS.PUT,
-  //     url: `${process.env.REACT_APP_MERAKI_URL}/users/calendar/tokens`,
-  //     headers: {
-  //       accept: "application/json",
-  //       Authorization: user.data.token,
-  //       code: code,
-  //     },
-  //   }).then((res) => {
-  //     if (res.data.success) setIsAuthenticated(res.data.success);
-  //     console.log("res", res);
-  //   });
-  // };
-
-  // if (url.includes("code")) {
-  //   code = url.split("code=")[1].split("scope")[0];
-  //   console.log("url", url.split("code=")[1].split("scope")[0]);
-  //   userCalender(code);
-  // }
-
-  // console.log("isAuthenticated", isAuthenticated);
-
-  // if (isAuthenticated) {
-  //   // return <Redirect to={PATHS.CLASS} />;
-  //   return (
-  //     <Redirect
-  //       to={{
-  //         pathname: `${PATHS.CLASS}`,
-  //         state: { toggle: isAuthenticated },
-  //       }}
-  //     />
-  //   );
-  // }
   return (
     <div className="ng-search">
       <input
