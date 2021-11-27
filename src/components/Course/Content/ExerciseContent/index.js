ce8fd00e713c77de37b9fb2b373df506e46eacd3
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2021-11-27 14:38:58 +0530

    Course content rendering

--- src/components/Course/Content/ExerciseContent/index.js
+++ src/components/Course/Content/ExerciseContent/index.js
@@ -9,14 +9,10 @@ import get from "lodash/get";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import HiddenContent from "../HiddenContent";
 import DOMPurify from "dompurify";
+import Highlight from "react-highlight";
 
 import "./styles.scss";
-
-// const createDOMPurify = require("dompurify");
 const { JSDOM } = require("jsdom");
-
-const window = new JSDOM("").window;
-// const DOMPurify = createDOMPurify(window);
 
 function getMarkdown(code, lang) {
   let l = lang == "python" ? "py" : "js";
@@ -41,43 +37,52 @@ const createVisulizeURL = (code, lang, mode) => {
   return url;
 };
 
-const components = {
-  code({ node, inline, className, children, ...props }) {
-    const match = /language-(\w+)/.exec(className || "");
-    return !inline && match ? (
-      <SyntaxHighlighter
-        showLineNumbers
-        language={match[1]}
-        PreTag="div"
-        children={String(children).replace(/\n$/, "")}
-        {...props}
-      />
-    ) : (
-      <code className={className} {...props}>
-        {children}
-      </code>
-    );
-  },
-};
-
-const table = {
-  table({ node, ...props }) {
-    return <table style={{ borderCollapse: "collapse" }} {...props} />;
-  },
-};
+// const components = {
+//   code({ node, inline, className, children, ...props }) {
+//     const match = /language-(\w+)/.exec(className || "");
+//     console.log("children", children);
+//     return !inline && match ? (
+//       <SyntaxHighlighter
+//         showLineNumbers
+//         language={match[1]}
+//         PreTag="div"
+//         children={String(children).replace(/\n$/, "")}
+//         {...props}
+//       />
+//     ) : (
+//       <code className={className} {...props}>
+//         {children}
+//       </code>
+//     );
+//   },
+// };
 
 const headingVarients = {
-  1: (data) => <h1 className="heading">{data.value}</h1>,
-  2: (data) => <h2 className="heading">{data.value}</h2>,
-  3: (data) => <h3 className="heading">{data.value}</h3>,
-  4: (data) => <h4 className="heading">{data.value}</h4>,
-  5: (data) => <h5 className="heading">{data.value}</h5>,
-  6: (data) => <h6 className="heading">{data.value}</h6>,
+  1: (data) => (
+    <h1 className="heading" dangerouslySetInnerHTML={{ __html: data }}></h1>
+  ),
+  2: (data) => (
+    <h2 className="heading" dangerouslySetInnerHTML={{ __html: data }}></h2>
+  ),
+  3: (data) => (
+    <h3 className="heading" dangerouslySetInnerHTML={{ __html: data }}></h3>
+  ),
+  4: (data) => (
+    <h4 className="heading" dangerouslySetInnerHTML={{ __html: data }}></h4>
+  ),
+  5: (data) => (
+    <h5 className="heading" dangerouslySetInnerHTML={{ __html: data }}></h5>
+  ),
+  6: (data) => (
+    <h6 className="heading" dangerouslySetInnerHTML={{ __html: data }}></h6>
+  ),
 };
 
 const RenderContent = ({ data }) => {
   if (data.component === "header") {
-    return headingVarients[data.variant](data);
+    return headingVarients[data.variant](
+      DOMPurify.sanitize(get(data, "value"))
+    );
   }
 
   if (data.component === "image") {
@@ -87,51 +92,92 @@ const RenderContent = ({ data }) => {
     return <YouTube className={"youtube-video"} videoId={data.value} />;
   }
   if (data.component === "text") {
-    return <p>{`${data.value}`}</p>;
-  }
-  // if (data.type === "markdown") {
-  //   return (
-  //     <ReactMarkdown
-  //       className="table-content"
-  //       children={data.value}
-  //       rehypePlugins={[rehypeRaw, rehypeSanitize]}
-  //       remarkPlugins={[gfm]}
-  //       components={table}
-  //     />
-  //   );
-  // }
-  if (data.component === "code")
-    if (data.type === "python" || data.type === "javascript") {
+    const text = DOMPurify.sanitize(get(data, "value"));
+    if (data.decoration && data.decoration.type === "bullet") {
       return (
-        <div>
-          <ReactMarkdown
-            components={components}
-            children={getMarkdown(
-              DOMPurify.sanitize(get(data, "value"), data.type)
-            )}
-          />
-          <div className="code__controls">
-            <a
-              target="_blank"
-              href={createVisulizeURL(get(data, "value"), data.type, "display")}
-            >
-              Visualize
-            </a>
-
-            <a
-              target="_blank"
-              href={createVisulizeURL(
-                get(data, "value.code"),
-                data.type,
-                "edit"
-              )}
-            >
-              Edit
-            </a>
-          </div>
-        </div>
+        <li className="paragraph" dangerouslySetInnerHTML={{ __html: text }} />
       );
     }
+    if (data.decoration && data.decoration.type === "number") {
+      return (
+        <div className="list">
+          <p
+            className="number"
+            dangerouslySetInnerHTML={{ __html: data.decoration.value }}
+          />
+          <p className="paragraph" dangerouslySetInnerHTML={{ __html: text }} />
+        </div>
+      );
+    } else {
+      return (
+        <p className="paragraph" dangerouslySetInnerHTML={{ __html: text }} />
+      );
+    }
+  }
+  if (data.component === "table") {
+    return (
+      <div>
+        <table className="table-data">
+          <thead>
+            <tr>
+              {data.value.map((item) => {
+                const header = DOMPurify.sanitize(item.header);
+                return <th dangerouslySetInnerHTML={{ __html: header }} />;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {data.value.map((item) => {
+              return (
+                <td>
+                  {item.items.map((row) => {
+                    const rowData = DOMPurify.sanitize(row);
+                    return (
+                      <>
+                        <div dangerouslySetInnerHTML={{ __html: rowData }} />
+                        <hr />
+                      </>
+                    );
+                  })}
+                </td>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  if (data.component === "code") {
+    // if (data.type === "python" || data.type === "javascript") {
+    const codeContent = DOMPurify.sanitize(get(data, "value"));
+    return (
+      <div>
+        <div className="code-bg">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: getMarkdown(codeContent, data.type),
+            }}
+          />
+          {/* <Highlight innerHTML={true}>{get(data, "value")}</Highlight> */}
+        </div>
+        <div className="code__controls">
+          <a
+            target="_blank"
+            href={createVisulizeURL(get(data, "value"), data.type, "display")}
+          >
+            Visualize
+          </a>
+
+          <a
+            target="_blank"
+            href={createVisulizeURL(get(data, "value.code"), data.type, "edit")}
+          >
+            Edit
+          </a>
+        </div>
+      </div>
+    );
+  }
   // if (data.type === "bash") {
   //   return (
   //     <code className="language-bash code-block">
@@ -159,6 +205,8 @@ function ExerciseContent(props) {
   if (!content) {
     return "";
   }
+
+  console.log("content", content);
 
   return (
     <div className="ng-exercise-content" align="justify">
