e2f2156f294a8220fa09f2c6af52347241eec7d3
jschanker <jschanker@molloy.edu>
2022-01-21 14:55:11 -0500

    Nested ternary => Boolean operators

--- src/components/Class/index.js
+++ src/components/Class/index.js
@@ -347,13 +347,7 @@ function Class({ classToEdit, indicator }) {
                       formFieldsState.type === "cohort" ? "checked" : false
                     }
                     // disabled={formFieldsState[TYPE] === "cohort" ? true : false}
-                    disabled={
-                      isEditMode
-                        ? formFieldsState[TYPE] === "cohort"
-                          ? false
-                          : true
-                        : false
-                    }
+                    disabled={isEditMode && formFieldsState[TYPE] !== "cohort"}
                   />
                   Cohort
                 </label>
@@ -370,13 +364,7 @@ function Class({ classToEdit, indicator }) {
                     checked={
                       formFieldsState.type === "doubt_class" ? "checked" : false
                     }
-                    disabled={
-                      isEditMode
-                        ? formFieldsState[TYPE] === "doubt_class"
-                          ? false
-                          : true
-                        : false
-                    }
+                    disabled={isEditMode && formFieldsState[TYPE] !== "doubt_class"}
                   />
                   Doubt Class
                 </label>
