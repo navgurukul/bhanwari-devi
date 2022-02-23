ccb0d5114957e71c6d3df5606ed5fc54067619fd
komalahire <komala@navgurukul.org>
2022-02-23 23:19:03 +0530

    Created Global CSS file.

--- src/routing/index.js
+++ src/routing/index.js
@@ -23,57 +23,62 @@ import VolunteerDashboard from "../components/VolunteerDashbord/VolunteerDetails
 import VolunteerOverview from "../components/VolunteerDashbord/IndividualVolunteers";
 import StatePartnerDashboard from "../pages/StatePartner Dashboard";
 
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme/theme";
+
 // import Classes from '../pages/classes'
 
 const Routing = () => {
   return (
     <>
-      {/* Public routes */}
-      {/* Route home page to login in  */}
-      <Route exact path={PATHS.HOME_PATH} component={Course} />
-      <Route exact path={PATHS.COURSE} component={Course} />
-      <Route path={PATHS.COURSE_CONTENT} component={CourseContent} />
-      <Route exact path={PATHS.LOGIN} component={Login} />
-      <Route exact path={PATHS.PRIVACY_POLICY} component={PrivacyPolicy} />
-      <Route exact path={PATHS.AFE} component={AFEpage} />
-      <Route exact path={PATHS.OPPORTUNITIES} component={Opportunities} />
-      <Route
-        exact
-        path={PATHS.NAVGURUKUL_INTRODUCE}
-        component={NavgurukulIntroduce}
-      />
-      <Route exact path={PATHS.REDIRECT} component={RedirectComponent} />
-      <PrivateRoute exact path={PATHS.ADMISSION} component={Admission} />
+      <ThemeProvider theme={theme}>
+        {/* Public routes */}
+        {/* Route home page to login in  */}
+        <Route exact path={PATHS.HOME_PATH} component={Course} />
+        <Route exact path={PATHS.COURSE} component={Course} />
+        <Route path={PATHS.COURSE_CONTENT} component={CourseContent} />
+        <Route exact path={PATHS.LOGIN} component={Login} />
+        <Route exact path={PATHS.PRIVACY_POLICY} component={PrivacyPolicy} />
+        <Route exact path={PATHS.AFE} component={AFEpage} />
+        <Route exact path={PATHS.OPPORTUNITIES} component={Opportunities} />
+        <Route
+          exact
+          path={PATHS.NAVGURUKUL_INTRODUCE}
+          component={NavgurukulIntroduce}
+        />
+        <Route exact path={PATHS.REDIRECT} component={RedirectComponent} />
+        <PrivateRoute exact path={PATHS.ADMISSION} component={Admission} />
 
-      {/* Private routes */}
-      {/* <PrivateRoute
+        {/* Private routes */}
+        {/* <PrivateRoute
         exact
         path={ PATHS.CLASS }
         component={ Class }
       />*/}
-      <PrivateRoute
-        exact
-        path={PATHS.STATEPARTNER}
-        component={StatePartnerDashboard}
-      />
-      <PrivateRoute exact path={PATHS.PROFILE} component={Profile} />
-      <PrivateRoute
-        exact
-        path={PATHS.VOLUNTEER}
-        component={VolunteerDashboard}
-      />
-      <PrivateRoute
-        exact
-        path={PATHS.VOLUNTEER_OVERVIEW}
-        component={VolunteerOverview}
-      />
+        <PrivateRoute
+          exact
+          path={PATHS.STATEPARTNER}
+          component={StatePartnerDashboard}
+        />
+        <PrivateRoute exact path={PATHS.PROFILE} component={Profile} />
+        <PrivateRoute
+          exact
+          path={PATHS.VOLUNTEER}
+          component={VolunteerDashboard}
+        />
+        <PrivateRoute
+          exact
+          path={PATHS.VOLUNTEER_OVERVIEW}
+          component={VolunteerOverview}
+        />
 
-      <PrivateRoute exact path={PATHS.CLASS} component={CreateClass} />
-      <PrivateRoute exact path={PATHS.USER} component={User} />
-      <PrivateRoute exact path={PATHS.MENTOR} component={Mentor} />
-      <PrivateRoute exact path={PATHS.PARTNERS} component={PartnerData} />
-      <PrivateRoute exact path={PATHS.PARTNER_DATA} component={StudentData} />
-      <PrivateRoute exact path={PATHS.STUDENT} component={StudentClassData} />
+        <PrivateRoute exact path={PATHS.CLASS} component={CreateClass} />
+        <PrivateRoute exact path={PATHS.USER} component={User} />
+        <PrivateRoute exact path={PATHS.MENTOR} component={Mentor} />
+        <PrivateRoute exact path={PATHS.PARTNERS} component={PartnerData} />
+        <PrivateRoute exact path={PATHS.PARTNER_DATA} component={StudentData} />
+        <PrivateRoute exact path={PATHS.STUDENT} component={StudentClassData} />
+      </ThemeProvider>
     </>
   );
 };
---
+++ src/theme/theme.js
@@ -0,0 +1,142 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#48a145",
+      light: "#e9f5e9",
+      dark: "#3a8137",
+    },
+    secondary: {
+      main: "#4548a1",
+      light: "#dadaec",
+      dark: "#373a81",
+    },
+    // error: {
+    //     main: '#f44336',
+    //     light: '#ffe5e3',
+    //     dark: '#c3362b',
+    // },
+    // caution: {
+    //     main: "#FFC107",
+    // },
+    // critical: {
+    //     main: "#F44336",
+    // }
+  },
+
+  // palette: {
+  //     type: 'light',
+  //     primary: {
+  //         main: '#48a145',
+  //         light: '#e9f5e9',
+  //         dark: '#3a8137',
+  //     },
+  //     secondary: {
+  //         main: '#4548a1',
+  //         light: '#dadaec',
+  //         dark: '#373a81',
+  //     },
+  //     error: {
+  //         main: '#f44336',
+  //         light: '#ffe5e3',
+  //         dark: '#c3362b',
+  //     },
+  //     warning: {
+  //         main: '#ffc107',
+  //         contrastText: '#2e2e2e',
+  //         light: '#fff3cd',
+  //         dark: '#cc9a06',
+  //     },
+  //     info: {
+  //         main: '#2196f3',
+  //         dark: '#1a78c2',
+  //         light: '#d3eafd',
+  //     },
+  //     success: {
+  //         main: '#48a145',
+  //         light: '#e9f5e9',
+  //         dark: '#3a8137',
+  //     },
+  //     text: {
+  //         primary: '#2e2e2e',
+  //         secondary: '#585858',
+  //         disabled: '#949494',
+  //         hint: '#949494',
+  //     },
+  //     background: {
+  //         default: '#f7f7f7',
+  //         paper: '#ffffff',
+  //     },
+  //     divider: '#DEDEDE',
+  // },
+  typography: {
+    fontFamily: "Nunito Sans",
+    // fontSize: 18,
+    //     h1: {
+    //         fontWeight: 700,
+    //         fontSize: '6rem',
+    //     },
+    //     h2: {
+    //         fontSize: '4.5rem',
+    //         fontWeight: 700,
+    //     },
+    //     h3: {
+    //         fontSize: '3.5rem',
+    //         fontWeight: 700,
+    //         fontFamily: 'Lusitana',
+    //         lineHeight: 1.14285714286,
+    //     },
+    //     h4: {
+    //         fontWeight: 700,
+    //         fontSize: '2.625rem',
+    //         fontFamily: 'Lusitana',
+    //         lineHeight: 1.33,
+    //     },
+    //     h5: {
+    //         fontWeight: 700,
+    //         fontSize: '2rem',
+    //         lineHeight: 1.33,
+    //         fontFamily: 'Lusitana',
+    //     },
+    //     h6: {
+    //         fontSize: '1.5rem',
+    //         lineHeight: 1.33,
+    //         fontWeight: 700,
+    //         fontFamily: 'Lusitana',
+    //     },
+    //     subtitle1: {
+    //         fontSize: '1.125rem',
+    //         lineHeight: 1.55,
+    //         fontWeight: 700,
+    //     },
+    //     subtitle2: {
+    //         fontSize: '0.875rem',
+    //         fontWeight: 700,
+    //         lineHeight: 1.42,
+    //     },
+    //     body1: {
+    //         fontSize: '1.125rem',
+    //         lineHeight: 1.55,
+    //     },
+    //     body2: {
+    //         fontSize: '0.875rem',
+    //         lineHeight: 1.42,
+    //     },
+    //     button: {
+    //         fontSize: '1.125rem',
+    //         lineHeight: 1.55,
+    //         fontWeight: 500,
+    //     },
+    //     caption: {
+    //         fontSize: '0.75rem',
+    //         lineHeight: 1.33,
+    //     },
+    //     overline: {
+    //         fontSize: '0.75rem',
+    //     },
+  },
+});
+
+export default theme;
