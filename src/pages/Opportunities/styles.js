17ab91b487f31c701a06dc8e56b111a0452530a6
amansharmma <aman18@navgurukul.org>
2022-04-02 16:56:30 +0530

    Opportunities

---
+++ src/pages/Opportunities/styles.js
@@ -0,0 +1,19 @@
+import { alignProperty } from "@mui/material/styles/cssUtils";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  section_Heading: {
+    margin: "20px 0px 17px 0px",
+  },
+
+  cards: {
+    maxWidth: "352px",
+  },
+
+  card_button: {
+    margin: "0px 0px 10px 0px",
+    align: "center",
+  },
+}));
+
+export default useStyles;
