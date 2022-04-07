ce8df619b43ee2f81cb76e63dca6a501e12808e8
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2022-04-07 22:32:36 +0530

    pathways

--- src/components/PathwayCourse/index.js
+++ src/components/PathwayCourse/index.js
@@ -20,11 +20,46 @@ import {
   Toolbar,
 } from "@mui/material";
 
-const content = [
-  "Vestibulum eu quam nec neque pellentesque efficitur id eget nisl. Proin porta est convallis lacus blandit",
-  "Vestibulum eu quam nec neque pellentesque efficitur id eget nisl. Proin porta est convallis lacus blandit",
-  "Vestibulum eu quam nec neque pellentesque efficitur id eget nisl. Proin porta est convallis lacus blandit",
-];
+const pathways = {
+  1: {
+    pathway: "Python",
+    description:
+      "Learn the basics and become comfortable in one of the most popular programming languages Python.",
+    outcomes: [
+      "Get equipped to build small projects like calculator or to-do list",
+      "Get the base knowledge to apply to advanced bootcamps such as Navgurukul or Zoho Schools",
+    ],
+  },
+
+  2: {
+    pathway: "Javascript",
+    description:
+      "Learn the nuances and basics of the technology that powers the web. Start with learning what is Javascript and eventually build your own website.",
+    outcomes: [
+      "Build your first web page and power it with the interactive language of Javascript",
+      "Build your basics of HTML, CSS and Javascript to prepare for advanced web development courses",
+    ],
+  },
+  3: {
+    pathway: "Typing Guru",
+    description:
+      "The typing track allows you to practice keyboard typing in a adaptive manner. You require a keyboard if on Android or use your laptop keyboard.",
+    outcomes: [
+      "Reach a typing speed of up to 30 to 40 words per minute",
+      "Be able to type long text with minimal inaccuracies",
+    ],
+  },
+  5: {
+    pathway: "Spoken English & Grammar",
+    description:
+      "English is a great tool needed to navigate the tech world and also in an International setting. Whether you are a total beginner or already know some English, prepare for the challenge with our Spoken English classes and online courses.",
+    outcomes: [
+      "Start speaking English without fear in about 6 months",
+      "Be able to read, write, listen and speak English with fluency",
+      "Be able to give oral presentations, talk to friends and prospective colleagues",
+    ],
+  },
+};
 
 const images = [
   "course1",
@@ -66,7 +101,7 @@ function PathwayCourse() {
   return (
     <>
       <Container maxWidth="lg">
-        <Grid container spacing={2} align="center" className={classes.box}>
+        {/* <Grid container spacing={2} align="center" className={classes.box}>
           <Grid xs={12} md={6}>
             <Card align="left" elevation={0} className={classes.titleCard}>
               <Typography
@@ -143,18 +178,64 @@ function PathwayCourse() {
               )}
             </Card>
           </Grid>
-          {/* <Grid xs={12} md={6} sx={{ pl: 2 }}>
-            <CardMedia
-              component="video"
-              autoPlay
-              controls
-              height="260"
-              src="https://www.youtube.com/watch?v=Doo1T5WabEU"
-              sx={{ width: isActive ? 380 : 480 }}
-            />
-          </Grid> */}
-        </Grid>
-        <Box className={classes.box}>
+        </Grid> */}
+        {pathwayId === pathwayId && (
+          <>
+            <Grid container spacing={2} align="center" className={classes.box}>
+              <Grid xs={12} md={6}>
+                <Card align="left" elevation={0} className={classes.titleCard}>
+                  <Typography
+                    variant="body2"
+                    className={classes.cardSubtitle}
+                    sx={{ textAlign: isActive && "center" }}
+                  >
+                    Learning Track
+                  </Typography>
+                  <Typography
+                    variant="h5"
+                    className={classes.heading}
+                    sx={{ textAlign: isActive && "center" }}
+                  >
+                    {pathways[pathwayId].pathway}
+                  </Typography>
+                  <Typography variant="body1">
+                    {pathways[pathwayId].description}
+                  </Typography>
+                </Card>
+              </Grid>
+              {/* <Grid xs={12} md={6} sx={{ pl: 2 }}>
+                <CardMedia
+                  component="video"
+                  autoPlay
+                  controls
+                  height="260"
+                  src="https://www.youtube.com/watch?v=Doo1T5WabEU"
+                  sx={{ width: isActive ? 380 : 480 }}
+                />
+              </Grid> */}
+            </Grid>
+            <Box className={classes.box}>
+              <Typography variant="h6" sx={{ textAlign: isActive && "center" }}>
+                Learning Outcomes
+              </Typography>
+              <Grid container spacing={0} align="center">
+                {pathways[pathwayId].outcomes.map((item) => (
+                  <Grid xs={12} md={4}>
+                    <Card align="left" elevation={0}>
+                      <Box className={classes.flex}>
+                        <CheckIcon color="primary" />
+                        <Typography sx={{ ml: 1 }} variant="body1">
+                          {item}
+                        </Typography>
+                      </Box>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+          </>
+        )}
+        {/* <Box className={classes.box}>
           <Typography variant="h6" sx={{ textAlign: isActive && "center" }}>
             Learning Outcomes
           </Typography>
@@ -172,7 +253,7 @@ function PathwayCourse() {
               </Grid>
             ))}
           </Grid>
-        </Box>
+        </Box> */}
         <Box className={classes.box}>
           <Typography
             className={classes.course}
