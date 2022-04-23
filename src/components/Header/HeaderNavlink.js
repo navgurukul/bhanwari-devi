eaefb53b863d9a7bc7c3d6f8fbe328ff3f815c48
komalahire <komala@navgurukul.org>
2022-04-24 00:42:33 +0530

    did some changes

--- src/components/Header/HeaderNavlink.js
+++ src/components/Header/HeaderNavlink.js
@@ -1,21 +1,7 @@
 import React from "react";
-import { PATHS, interpolatePath } from "../../constant";
-import { hasOneFrom } from "../../common/utils";
-import { actions as userActions } from "../User/redux/action";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import useStyles from "./styles";
-import { DropDown, MobileDropDown } from "./DropDown";
-import { sendToken } from "../User/redux/api";
-import { actions as pathwayActions } from "../../components/PathwayCourse/redux/action";
-import {
-  Box,
-  IconButton,
-  Typography,
-  Menu,
-  Avatar,
-  MenuItem,
-} from "@mui/material";
+
+import { Typography, MenuItem } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 function HeaderNavLink(props) {
