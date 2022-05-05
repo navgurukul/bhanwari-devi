41d252a0f301713fc66778b9086c4a29d91d737a
komalahire <komala@navgurukul.org>
2022-05-05 13:51:39 +0530

    api integration

--- src/pages/PartnerNew/index.js
+++ src/pages/PartnerNew/index.js
@@ -7,9 +7,17 @@ import {
   Card,
   Stack,
   Box,
+  CardHeader,
+  CardContent,
+  CardActions,
   Grid,
 } from "@mui/material";
 import useStyles from "./styles";
+import axios from "axios";
+import PublicIcon from "@mui/icons-material/Public";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import IconButton from "@mui/material/IconButton";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { breakpoints } from "../../theme/constant";
 import { Link } from "react-router-dom";
@@ -20,6 +28,16 @@ import ChevronRightOutlinedIcon from "@mui/icons-material/ChevronRightOutlined";
 function NewParnter() {
   const classes = useStyles();
   const isActive = useMediaQuery("(max-width:" + breakpoints.values.sm + "px)");
+  const [partners, setPartners] = useState([]);
+
+  useEffect(() => {
+    axios({
+      url: `https://anandpatel504.github.io/tarabai-shinde/data/meraki_partners.json`,
+    }).then((res) => {
+      setPartners(res.data);
+    });
+  }, []);
+
   return (
     <Container maxWidth="lg">
       <Container maxWidth="sm">
@@ -47,7 +65,7 @@ function NewParnter() {
         <Grid container spacing={{ xs: 2, sm: 4 }}>
           <Grid item xs={12} sm={6} md={6}>
             <img
-              className={classes.playstoreImg}
+              // className={classes.playstoreImg}
               src={require("./assest/partnerLogo.svg")}
             />
           </Grid>
@@ -100,6 +118,204 @@ function NewParnter() {
         Partners List
       </Typography>
       <hr color="primary" className={classes.underLine} />
+      <Grid container spacing={5} className={classes.partnerBottomspacing}>
+        {Object.keys(partners).length ? (
+          Object.keys(partners).map((item) => {
+            return (
+              <Grid item xs={12} sm={4} md={4}>
+                {!partners[item].Name == "" &&
+                  !partners[item].OrganisationType == "" &&
+                  !partners[item].State == "" &&
+                  !partners[item].City == "" && (
+                    <Card className={classes.partnerCard}>
+                      <CardHeader
+                        title={partners[item].Name}
+                        titleTypographyProps={{ variant: "subtitle1" }}
+                        className={classes.partnerCardContainer}
+                      />
+                      <CardContent
+                        className={
+                          !isActive
+                            ? classes.partnerCardContainer
+                            : classes.partnerCardContainer1
+                        }
+                      >
+                        {partners[item].OrganisationType == "Non - Profit" ? (
+                          <Button
+                            variant="contained"
+                            rounded
+                            sx={
+                              !isActive
+                                ? {
+                                    borderRadius: { xs: 25, sm: 15 },
+                                    height: { xs: "60", sm: "30px" },
+                                    fontSize: "caption",
+                                    background: "lemonchiffon",
+                                    color: "black",
+                                    marginTop: "-20px",
+                                    marginBottom: "15px",
+                                  }
+                                : {
+                                    borderRadius: { xs: 25, sm: 15 },
+                                    height: { xs: 34, sm: 25 },
+                                    size: "small",
+                                    fontSize: "caption",
+                                    background: "lemonchiffon",
+                                    color: "black",
+                                    marginTop: "-20px",
+                                    marginBottom: "15px",
+                                  }
+                            }
+                          >
+                            {partners[item].OrganisationType}
+                          </Button>
+                        ) : partners[item].OrganisationType ==
+                          "Educational Institution" ? (
+                          <Button
+                            variant="contained"
+                            rounded
+                            sx={
+                              !isActive
+                                ? {
+                                    borderRadius: { xs: 25, sm: 15 },
+                                    height: { xs: "60", sm: "30px" },
+                                    fontSize: "caption",
+                                    background: "lightskyblue",
+                                    color: "black",
+                                    marginTop: "-20px",
+                                    marginBottom: "15px",
+                                  }
+                                : {
+                                    borderRadius: { xs: 25, sm: 15 },
+                                    height: { xs: 34, sm: 25 },
+                                    size: "small",
+                                    fontSize: "caption",
+                                    background: "lightskyblue",
+                                    color: "black",
+                                    marginTop: "-20px",
+                                    marginBottom: "15px",
+                                  }
+                            }
+                          >
+                            {partners[item].OrganisationType}
+                          </Button>
+                        ) : partners[item].OrganisationType == "Government" ? (
+                          <Button
+                            variant="contained"
+                            rounded
+                            sx={
+                              !isActive
+                                ? {
+                                    borderRadius: { xs: 25, sm: 15 },
+                                    height: { xs: "60", sm: "30px" },
+                                    fontSize: "caption",
+                                    background: "silver",
+                                    color: "black",
+                                    marginTop: "-20px",
+                                    marginBottom: "15px",
+                                  }
+                                : {
+                                    borderRadius: { xs: 25, sm: 15 },
+                                    height: { xs: 34, sm: 25 },
+                                    size: "small",
+                                    fontSize: "caption",
+                                    background: "silver",
+                                    color: "black",
+                                    marginTop: "-20px",
+                                    marginBottom: "15px",
+                                  }
+                            }
+                          >
+                            {partners[item].OrganisationType}
+                          </Button>
+                        ) : partners[item].OrganisationType ==
+                          "Community based organisation" ? (
+                          <Button
+                            variant="contained"
+                            rounded
+                            sx={
+                              !isActive
+                                ? {
+                                    borderRadius: { xs: 25, sm: 15 },
+                                    height: { xs: "60", sm: "30px" },
+                                    fontSize: "caption",
+                                    color: "black",
+                                    marginTop: "-20px",
+                                    marginBottom: "15px",
+                                  }
+                                : {
+                                    borderRadius: { xs: 25, sm: 15 },
+                                    height: { xs: 34, sm: 25 },
+                                    size: "small",
+                                    fontSize: "caption",
+                                    color: "black",
+                                    marginTop: "-20px",
+                                    marginBottom: "15px",
+                                  }
+                            }
+                          >
+                            {partners[item].OrganisationType}
+                          </Button>
+                        ) : (
+                          ""
+                        )}
+
+                        <Typography variant="body1">
+                          {!partners[item].City == "" &&
+                            `${partners[item].City}, `}
+                          {partners[item].State}
+                        </Typography>
+                      </CardContent>
+                      <CardActions>
+                        {partners[item].Url ? (
+                          <IconButton>
+                            <Link href={partners[item].Url}>
+                              <PublicIcon
+                                variant="outlined"
+                                fontSize="small"
+                                className={classes.partnerIconSize}
+                              />
+                            </Link>
+                          </IconButton>
+                        ) : (
+                          <></>
+                        )}
+                        {partners[item].LinkedinId ? (
+                          <IconButton>
+                            <Link href={partners[item].LinkedinId}>
+                              <LinkedInIcon
+                                variant="outlined"
+                                fontSize="small"
+                                className={classes.partnerIconSize}
+                              />
+                            </Link>
+                          </IconButton>
+                        ) : (
+                          <></>
+                        )}
+                        {partners[item].TwitterId ? (
+                          <IconButton>
+                            <Link href={partners[item].TwitterId}>
+                              <TwitterIcon
+                                variant="outlined"
+                                fontSize="small"
+                                className={classes.partnerIconSize}
+                              />
+                            </Link>
+                          </IconButton>
+                        ) : (
+                          <></>
+                        )}
+                      </CardActions>
+                    </Card>
+                  )}
+              </Grid>
+            );
+          })
+        ) : (
+          <></>
+        )}
+      </Grid>
     </Container>
   );
 }
