5cb5b80d29f8123fee10eadeb0bdb5564aba1140
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2022-05-19 21:02:20 +0530

    added partner id field to the class create form

--- src/components/Class/index.js
+++ src/components/Class/index.js
@@ -39,12 +39,9 @@ function Class({ classToEdit, indicator }) {
   const [loading, setLoading] = useState(false);
   const [pathwayId, setPathwayId] = useState();
   const [checkedState, setCheckedState] = useState(new Array(7).fill(false));
-  const [partnerName, setPartnerName] = useState();
   const [partnerData, setPartnerData] = useState([]);
-  const [autoCompleteValue, setAutoCompleteValue] = useState({
-    label: "",
-    value: "",
-  });
+  const [Selected_partner_id, setSelected_partner_id] = useState();
+
   const {
     title,
     description,
@@ -98,7 +95,7 @@ function Class({ classToEdit, indicator }) {
       [UNTIL]: until_data || "",
     };
   }, [classToEdit]);
-  const [Selected_partner_id, setSelected_partner_id] = useState();
+
   const user = useSelector(({ User }) => User);
   const rolesList = user.data.user.rolesList;
 
@@ -164,7 +161,6 @@ function Class({ classToEdit, indicator }) {
   }, []);
 
   useEffect(() => {
-    // if (partnerName && partnerName.length > 3) {
     axios({
       method: METHODS.GET,
       url: `${process.env.REACT_APP_MERAKI_URL}/partners`,
@@ -177,12 +173,11 @@ function Class({ classToEdit, indicator }) {
       const partners = res.data.partners.map((item, index) => {
         return {
           label: item.name,
-          value: item.id,
+          id: item.id,
         };
       });
       setPartnerData(partners);
     });
-    // }
   }, []);
 
   const onCourseChange = (courseId) => {
@@ -242,7 +237,9 @@ function Class({ classToEdit, indicator }) {
     payload[TIME_CONSTANT.CLASS_END_DATE] = `${moment(classEndTime).format(
       "YYYY-MM-DDTHH:mm:ss"
     )}Z`;
+
     payload[PARTNER_ID] = Selected_partner_id;
+
     if (!isEditMode) {
       createClass(payload);
     } else {
@@ -305,7 +302,7 @@ function Class({ classToEdit, indicator }) {
           position: toast.POSITION.BOTTOM_RIGHT,
         });
         setLoading(false);
-        // window.location.reload(1);
+        window.location.reload(1);
       },
       (error) => {
         toast.error(
@@ -344,10 +341,9 @@ function Class({ classToEdit, indicator }) {
           formFields[fieldName] = value.split(",");
         }
         if (fieldName === "pathway_id") {
-          formFields[fieldName] = parseInt(value);
-          console.log("type", typeof value, "value", value);
-          // console.log("removing pathway_id from payload");
-          // continue;
+          // formFields[fieldName] = parseInt(value);
+          console.log("removing pathway_id from payload");
+          continue;
         } else {
           formFields[fieldName] = value;
         }
@@ -582,77 +578,23 @@ function Class({ classToEdit, indicator }) {
                   />
                 </>
               )}
-              {/* <Autocomplete
-                options={partnerData && partnerData}
-                value={autoCompleteValue}
-                getOptionLabel={(option) => option.label}
-                renderOption={(option) => <>{option.label}</>}
-                onChange={(event, newValue) => {
-                  setAutoCompleteValue(newValue);
-
-                  // setValue(newValue);
-                  // setCustomValue(newValue.id);
-                  // setPartnerName(newValue);
-                  setFormFieldsState({
-                    ...formFieldsState,
-                    [PARTNER_ID]: autoCompleteValue.value,
-                  });
-                  // }}
-                  // inputValue={inputValue}
-                  // onInputChange={(event, newInputValue) => {
-                  //   setInputValue(newInputValue);
-                }}
-                style={{ width: 300 }}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label="Controllable"
-                    variant="outlined"
-                  />
-                )}
-              /> */}
               <Autocomplete
                 disablePortal
                 id="combo-box-demo"
                 options={partnerData && partnerData}
-                // name={PARTNER_ID}
-                // value={partnerName}
-                // onChange={(e, newVal) => {
-                //   console.log("e", e.target.value);
-                //   // setFormFieldsState(newVal.value, )
-                //   setPartnerName(newVal);
-                // setFormFieldsState({
-                //   ...formFieldsState,
-                //   [PARTNER_ID]: newVal.value,
-                // });
-                // changeHandler(newVal.value, setFormFieldsState, formFieldsState);
-                // }}
-
                 isOptionEqualToValue={(option, value) => {
-                  return option.value === value.value;
+                  return option.id === value.id;
                 }}
                 onChange={(e, newVal) => {
-                  setSelected_partner_id(newVal.value);
+                  setSelected_partner_id(newVal.id);
                 }}
                 name={PARTNER_ID}
-                sx={{ width: 300 }}
+                sx={{ width: 300, mb: "30px" }}
                 renderInput={(params) => (
                   <TextField
                     {...params}
                     label="Search Partner Name"
                     variant="standard"
-
-                    // onChange={(e, newVal) => {
-                    //   // console.log("e", e.target);
-                    //   // setPartnerName(newVal);
-                    //   // setFormFieldsState({
-                    //   //   ...formFieldsState,
-                    //   //   [PARTNER_ID]: newVal.value,
-                    //   // });
-                    //   console.log(e.target);
-                    //   changeHandler(e, setFormFieldsState, formFieldsState);
-                    // }}
-                    // id={}
                   />
                 )}
               />
@@ -1148,131 +1090,3 @@ function Class({ classToEdit, indicator }) {
   );
 }
 export default Class;
-
-const top100Films = [
-  { label: "The Shawshank Redemption", year: 1994 },
-  { label: "The Godfather", year: 1972 },
-  { label: "The Godfather: Part II", year: 1974 },
-  { label: "The Dark Knight", year: 2008 },
-  { label: "12 Angry Men", year: 1957 },
-  { label: "Schindler's List", year: 1993 },
-  { label: "Pulp Fiction", year: 1994 },
-  {
-    label: "The Lord of the Rings: The Return of the King",
-    year: 2003,
-  },
-  { label: "The Good, the Bad and the Ugly", year: 1966 },
-  { label: "Fight Club", year: 1999 },
-  {
-    label: "The Lord of the Rings: The Fellowship of the Ring",
-    year: 2001,
-  },
-  {
-    label: "Star Wars: Episode V - The Empire Strikes Back",
-    year: 1980,
-  },
-  { label: "Forrest Gump", year: 1994 },
-  { label: "Inception", year: 2010 },
-  {
-    label: "The Lord of the Rings: The Two Towers",
-    year: 2002,
-  },
-  { label: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { label: "Goodfellas", year: 1990 },
-  { label: "The Matrix", year: 1999 },
-  { label: "Seven Samurai", year: 1954 },
-  {
-    label: "Star Wars: Episode IV - A New Hope",
-    year: 1977,
-  },
-  { label: "City of God", year: 2002 },
-  { label: "Se7en", year: 1995 },
-  { label: "The Silence of the Lambs", year: 1991 },
-  { label: "It's a Wonderful Life", year: 1946 },
-  { label: "Life Is Beautiful", year: 1997 },
-  { label: "The Usual Suspects", year: 1995 },
-  { label: "Léon: The Professional", year: 1994 },
-  { label: "Spirited Away", year: 2001 },
-  { label: "Saving Private Ryan", year: 1998 },
-  { label: "Once Upon a Time in the West", year: 1968 },
-  { label: "American History X", year: 1998 },
-  { label: "Interstellar", year: 2014 },
-  { label: "Casablanca", year: 1942 },
-  { label: "City Lights", year: 1931 },
-  { label: "Psycho", year: 1960 },
-  { label: "The Green Mile", year: 1999 },
-  { label: "The Intouchables", year: 2011 },
-  { label: "Modern Times", year: 1936 },
-  { label: "Raiders of the Lost Ark", year: 1981 },
-  { label: "Rear Window", year: 1954 },
-  { label: "The Pianist", year: 2002 },
-  { label: "The Departed", year: 2006 },
-  { label: "Terminator 2: Judgment Day", year: 1991 },
-  { label: "Back to the Future", year: 1985 },
-  { label: "Whiplash", year: 2014 },
-  { label: "Gladiator", year: 2000 },
-  { label: "Memento", year: 2000 },
-  { label: "The Prestige", year: 2006 },
-  { label: "The Lion King", year: 1994 },
-  { label: "Apocalypse Now", year: 1979 },
-  { label: "Alien", year: 1979 },
-  { label: "Sunset Boulevard", year: 1950 },
-  {
-    label:
-      "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
-    year: 1964,
-  },
-  { label: "The Great Dictator", year: 1940 },
-  { label: "Cinema Paradiso", year: 1988 },
-  { label: "The Lives of Others", year: 2006 },
-  { label: "Grave of the Fireflies", year: 1988 },
-  { label: "Paths of Glory", year: 1957 },
-  { label: "Django Unchained", year: 2012 },
-  { label: "The Shining", year: 1980 },
-  { label: "WALL·E", year: 2008 },
-  { label: "American Beauty", year: 1999 },
-  { label: "The Dark Knight Rises", year: 2012 },
-  { label: "Princess Mononoke", year: 1997 },
-  { label: "Aliens", year: 1986 },
-  { label: "Oldboy", year: 2003 },
-  { label: "Once Upon a Time in America", year: 1984 },
-  { label: "Witness for the Prosecution", year: 1957 },
-  { label: "Das Boot", year: 1981 },
-  { label: "Citizen Kane", year: 1941 },
-  { label: "North by Northwest", year: 1959 },
-  { label: "Vertigo", year: 1958 },
-  {
-    label: "Star Wars: Episode VI - Return of the Jedi",
-    year: 1983,
-  },
-  { label: "Reservoir Dogs", year: 1992 },
-  { label: "Braveheart", year: 1995 },
-  { label: "M", year: 1931 },
-  { label: "Requiem for a Dream", year: 2000 },
-  { label: "Amélie", year: 2001 },
-  { label: "A Clockwork Orange", year: 1971 },
-  { label: "Like Stars on Earth", year: 2007 },
-  { label: "Taxi Driver", year: 1976 },
-  { label: "Lawrence of Arabia", year: 1962 },
-  { label: "Double Indemnity", year: 1944 },
-  {
-    label: "Eternal Sunshine of the Spotless Mind",
-    year: 2004,
-  },
-  { label: "Amadeus", year: 1984 },
-  { label: "To Kill a Mockingbird", year: 1962 },
-  { label: "Toy Story 3", year: 2010 },
-  { label: "Logan", year: 2017 },
-  { label: "Full Metal Jacket", year: 1987 },
-  { label: "Dangal", year: 2016 },
-  { label: "The Sting", year: 1973 },
-  { label: "2001: A Space Odyssey", year: 1968 },
-  { label: "Singin' in the Rain", year: 1952 },
-  { label: "Toy Story", year: 1995 },
-  { label: "Bicycle Thieves", year: 1948 },
-  { label: "The Kid", year: 1921 },
-  { label: "Inglourious Basterds", year: 2009 },
-  { label: "Snatch", year: 2000 },
-  { label: "3 Idiots", year: 2009 },
-  { label: "Monty Python and the Holy Grail", year: 1975 },
-];
