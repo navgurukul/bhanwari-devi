b182397339f8f8960f9a64b325c2ec840514e63f
jschanker <jschanker@molloy.edu>
2022-06-20 15:36:45 -0400

    Refactor tests/use forEach instead of for loop

--- src/components/Class/ClassJoinTimerButton/ClassJoinTimerButton.test.js
+++ src/components/Class/ClassJoinTimerButton/ClassJoinTimerButton.test.js
@@ -6,40 +6,46 @@ import ClassJoinTimerButton from "./";
 // jest.setSystemTime
 
 describe("ClassJoinTimerButton", () => {
-  // Joining should not be allowed until at least 10 minutes before class start
-  it("Button should be disabled more than 10 minutes before class start", () => {
+  const MINUTE = 60; // in seconds
+  const HOUR = 60 * MINUTE;
+  const DAY = 24 * HOUR;
+  const YEAR = 365 * DAY;
+
+  const checkDisabled = (testTimes, expectDisabled) => {
     const { rerender, getByRole } = render(<ClassJoinTimerButton />);
-    //const timerButton = getByRole("button");
-    const MINUTE = 60; // in seconds
-    const HOUR = 60 * MINUTE;
-    const DAY = 24 * HOUR;
-    const YEAR = 365 * DAY;
 
-    const testTimes = [
-      20 * MINUTE,
-      7 * HOUR,
-      DAY + 3 * HOUR + 5 * MINUTE,
-      2 * YEAR + HOUR,
-      YEAR,
-      11 * MINUTE + Math.floor(Math.random() * 60)
-    ];
-
-    for (let i = 0; i < testTimes.length; i++) {
+    testTimes.forEach((testTime) => {
       rerender(
         <ClassJoinTimerButton
           //startTime="2022-06-21T03:25:00.000+05:30"
           startTime={
-            new Date(
-              new Date().setSeconds(new Date().getSeconds() + testTimes[i])
-            )
+            new Date(new Date().setSeconds(new Date().getSeconds() + testTime))
           }
           link=""
           joinOnClick={() => true}
         />
       );
 
-      expect(getByRole("button")).toBeDisabled();
-    }
+      expect(getByRole("button"))[
+        "toBe" + (expectDisabled ? "Disabled" : "Enabled")
+      ]();
+    });
+  };
+
+  // Joining should not be allowed until at least 10 minutes before class start
+  it("Button should be disabled more than 10 minutes before class start", () => {
+    checkDisabled(
+      [
+        20 * MINUTE,
+        7 * HOUR,
+        DAY + 3 * HOUR + 5 * MINUTE,
+        2 * YEAR + HOUR,
+        YEAR,
+        11 * MINUTE + Math.floor(Math.random() * 60)
+      ],
+      true
+    );
+
     //expect(getAllByRole("button")[index]).toBeDisabled();
 
     //expect(getByText('Class in')).toBeDisabled();
@@ -53,36 +59,17 @@ describe("ClassJoinTimerButton", () => {
 
   // Joining should be allowed 10 minutes before class start or later
   it("Button should be enabled 10 minutes before class start or later", () => {
-    const { rerender, getByRole } = render(<ClassJoinTimerButton />);
-    //const timerButton = getByRole("button");
-    const MINUTE = 60; // in seconds
-    const HOUR = 60 * MINUTE;
-
-    const testTimes = [
-      10 * MINUTE,
-      -10 * MINUTE,
-      0,
-      -5 * MINUTE + Math.floor(Math.random() * 60),
-      -30 * MINUTE + Math.floor(Math.random() * 60),
-      Math.floor(Math.random() * 60),
-      9 * MINUTE + Math.floor(Math.random() * 60)
-    ];
-
-    for (let i = 0; i < testTimes.length; i++) {
-      rerender(
-        <ClassJoinTimerButton
-          //startTime="2022-06-21T03:25:00.000+05:30"
-          startTime={
-            new Date(
-              new Date().setSeconds(new Date().getSeconds() + testTimes[i])
-            )
-          }
-          link=""
-          joinOnClick={() => true}
-        />
-      );
-
-      expect(getByRole("button")).toBeEnabled();
-    }
+    checkDisabled(
+      [
+        10 * MINUTE,
+        -10 * MINUTE,
+        0,
+        -5 * MINUTE + Math.floor(Math.random() * 60),
+        -30 * MINUTE + Math.floor(Math.random() * 60),
+        Math.floor(Math.random() * 60),
+        9 * MINUTE + Math.floor(Math.random() * 60)
+      ],
+      false
+    );
   });
 });
