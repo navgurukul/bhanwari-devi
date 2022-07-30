5414dbe23589098bb6bceb3fe753a38d70287fde
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2022-07-30 17:25:22 +0530

    success model + max_enrolnment

--- src/components/Class/SuccessModel.js
+++ src/components/Class/SuccessModel.js
@@ -5,14 +5,23 @@ import useStyles from "./styles";
 function SuccessModel() {
   const classes = useStyles();
   return (
-    <Stack align="center" className={classes.successModel}>
-      <Box className={classes.ModelBox} sx={{ bgcolor: "background.paper" }}>
-        <img alt="img" src={require("./assets/ClassCreated.svg")} />
-        <Typography variant="body1" mt={4}>
-          The batch has been created successfully
-        </Typography>
-      </Box>
-    </Stack>
+    <div
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <Stack align="center" className={classes.successModel}>
+        <Box className={classes.ModelBox} sx={{ bgcolor: "background.paper" }}>
+          <img alt="img" src={require("./assets/ClassCreated.svg")} />
+          <Typography variant="body1" mt={4}>
+            The batch has been created successfully
+          </Typography>
+        </Box>
+      </Stack>
+    </div>
   );
 }
 
