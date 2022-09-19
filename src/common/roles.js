e8a51e4b5910cc2d587092a59ceee1f499f49d52
jschanker <jschanker@molloy.edu>
2022-09-19 08:58:26 -0400

    Add landing page/selected role properties
    * `useRoles` hook modified to `return`s `Array` of roles with landing pages and Booleans representing whether the role is currently selected (i.e., whether it's the current view)
    * Add `saveSelectedRole` function to save selected `roleKey` for the given user id in `localStorage`.

--- src/common/roles.js
+++ src/common/roles.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { PATHS } from "../constant";
 
 export const ADMIN_ROLE_KEY = "ADMIN";
 export const STUDENT_ROLE_KEY = "STUDENT";
@@ -7,21 +8,26 @@ export const PARTNER_ROLE_KEY = "PARTNER";
 export const PARTNER_VIEW_ROLE_KEY = "PARTNER_VIEW";
 export const PARTNER_EDIT_ROLE_KEY = "PARTNER_EDIT";
 
+const ID_TO_SELECTED_ROLE_MAP_KEY = "idToSelectedRoleMap";
+
 const ROLES = {
   [ADMIN_ROLE_KEY]: {
     msgKey: ADMIN_ROLE_KEY,
     savedValue: "admin",
     isDefault: false,
+    landingPage: PATHS.PARTNERS,
   },
   [STUDENT_ROLE_KEY]: {
     msgKey: STUDENT_ROLE_KEY,
     savedValue: "student",
     isDefault: true,
+    landingPage: PATHS.NEW_USER_DASHBOARD,
   },
   [VOLUNTEER_ROLE_KEY]: {
     msgKey: VOLUNTEER_ROLE_KEY,
     savedValue: "volunteer",
     isDefault: false,
+    landingPage: PATHS.CLASS,
   },
   [PARTNER_ROLE_KEY]: {
     msgKey: PARTNER_ROLE_KEY,
@@ -31,6 +37,7 @@ const ROLES = {
       partnerId: null,
       partnerGroupId: null,
     },
+    landingPage: PATHS.PARTNERS,
   },
   [PARTNER_VIEW_ROLE_KEY]: {
     msgKey: PARTNER_ROLE_KEY,
@@ -44,8 +51,9 @@ const ROLES = {
   },
 };
 
+
 // keys of roles which every user has
-const DEFAULT_ROLES = Object.keys(ROLES).filter(
+const DEFAULT_ROLE_KEYS = Object.keys(ROLES).filter(
   (role) => ROLES[role].isDefault
 );
 
@@ -55,38 +63,70 @@ const savedRolesToKeysMap = Object.keys(ROLES).reduce((roleKeyMap, roleKey) => {
 }, {});
 
 /**
- * Custom hook to get user roles
- * @return {Array.<{key: string, msgKey: string, assignedRole:boolean}>}
+ * Custom hook to get user roles with relevant information
+ * @return {Array.<{key: string, msgKey: string, assignedRole: boolean, selected: boolean, properties: Object, landingPage: string|undefined}>}
  */
 export const useRoles = () => {
   const user = useSelector(({ User }) => User);
-  const rolesList = (user.data.user.rolesList || []).map(
+  const roleKeys = (user?.data?.user?.rolesList || []).map(
     (savedRole) => savedRolesToKeysMap[savedRole] || savedRole
   );
-  const unassignedDefaultRoles = DEFAULT_ROLES.filter(
-    (roleKey) => !rolesList.includes(roleKey)
+  const unassignedDefaultRoleKeys = DEFAULT_ROLE_KEYS.filter(
+    (roleKey) => !roleKeys.includes(roleKey)
   );
+  const defaultRoleKey = roleKeys[0] || unassignedDefaultRoleKeys;
+  const uid = user?.data?.user?.id;
+  const lastSelectedRoleKey = JSON.parse(
+    localStorage.getItem(ID_TO_SELECTED_ROLE_MAP_KEY)
+  )?.[uid]; // || localStorage.getItem(SELECTED_ROLE_KEY);
+  const hasLastSelectedRole = 
+    roleKeys.concat(unassignedDefaultRoleKeys).includes(lastSelectedRoleKey);
+  const selectedRoleKey = hasLastSelectedRole ? lastSelectedRoleKey : defaultRoleKey;
 
   // special case for partner role
-  // const partnerId = user.data.user.partner_id;
-  // const partnerGroupId = user.data.user.partner_group_id;
-  ROLES[PARTNER_ROLE_KEY].properties.partnerId = user.data.user.partner_id;
-  ROLES[PARTNER_ROLE_KEY].properties.partnerGroupId =
-    user.data.user.partner_group_id;
+  const partnerId = user?.data?.user?.partner_id;
+  const partnerGroupId = user?.data?.user?.partner_group_id;
+  
+  // HACK: selector should not mutate object 
+  ROLES[PARTNER_ROLE_KEY].properties.partnerId = partnerId;
+  ROLES[PARTNER_ROLE_KEY].properties.partnerGroupId = partnerGroupId;
+  ROLES[PARTNER_ROLE_KEY].landingPage = partnerGroupId
+    ? `${PATHS.STATE}/${partnerGroupId}`
+    : `${PATHS.PARTNERS}/${partnerId || ""}`,
+  // END HACK
 
-  return unassignedDefaultRoles
+  return unassignedDefaultRoleKeys
     .map((roleKey) => ({
       key: roleKey,
       msgKey: ROLES[roleKey].msgKey,
       assignedRole: false,
-      properties: ROLES[roleKey].properties,
+      properties: {...ROLES[roleKey].properties},
+      landingPage: ROLES[roleKey].landingPage,
+      selected: selectedRoleKey === roleKey,
     }))
     .concat(
       rolesList.map((roleKey) => ({
         key: roleKey,
         msgKey: ROLES[roleKey].msgKey,
         assignedRole: true,
-        properties: ROLES[roleKey].properties,
+        properties: {...ROLES[roleKey].properties},
+        landingPage: ROLES[roleKey].landingPage,
+        selected: selectedRoleKey === roleKey,
       }))
     );
 };
+
+/**
+ * Saves the specified role key in localStorage to use as the view upon refresh/login
+ * @param {string} roleKey - the key of the role to save
+ * @param {string} uid - the id of the user
+ */
+export const saveSelectedRole(roleKey, uid) {
+  const idToSelectedRoleMap =
+    JSON.parse(localStorage.getItem(ID_TO_SELECTED_ROLE_MAP_KEY)) || {};
+  idToSelectedRoleMap[uid] = roleKey;
+  localStorage.setItem(
+    ID_TO_SELECTED_ROLE_MAP_KEY,
+    JSON.stringify(idToSelectedRoleMap)
+  );
+}
