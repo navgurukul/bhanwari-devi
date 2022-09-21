89fe2bd92a9a4fd44968769b0fb1baa168a7998f
Kirithiv R <52394696+kirithiv2000@users.noreply.github.com>
2022-09-21 10:35:06 +0530

    Feat/content editor (#713)
    * question code editable
    
    * edit course converted to MUI
    
    * Improved style of editing header
    
    * course editing header text image and youtube
    
    * added textarea and changes the form style
    
    Co-authored-by: Poonam-Singh-Bagh <poonambagh98@gmail.com>

--- src/components/PathwayExercise/index.js
+++ src/components/PathwayExercise/index.js
@@ -25,6 +25,7 @@ import {
   Select,
   MenuItem,
 } from "@mui/material";
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 import CompletionComponent from "./CourseCompletion/CompletionComponent";
 import ExerciseImage from "./ExerciseImage/ExerciseImage.js";
 
@@ -131,6 +132,8 @@ function PathwayExercise() {
   const [showArrow, setShowArrow] = useState({ left: false, right: true });
   const currentCourse = params.courseId;
   const scrollRef = React.useRef();
+
+  const editor = "editor-role";
 
   const onScroll = () => {
     const scrollY = scrollRef.current.scrollLeft; //Don't get confused by what's scrolling - It's not the window
@@ -504,6 +507,45 @@ function PathwayExercise() {
           </div>
         </Container>
       </AppBar>
+      {editor && (
+        <AppBar
+          fullWidth
+          position="sticky"
+          sx={{ bgcolor: "info.light" }}
+          elevation={2}
+        >
+          <Box>
+            <Container maxWidth>
+              <Toolbar sx={{ alignItems: "center" }}>
+                <Box sx={{ flexGrow: 1 }} />
+                <ModeEditOutlineOutlinedIcon
+                  className={classes.edit}
+                  sx={{ mr: "11px" }}
+                />
+                <Typography className={classes.edit}>
+                  Want to update the content?
+                </Typography>
+                <Button
+                  sx={{ color: "#000000", ml: "24px" }}
+                  className={classes.edit}
+                  onClick={() => {
+                    history.push(
+                      interpolatePath(PATHS.PATHWAY_COURSE_CONTENT_EDIT, {
+                        courseId: params.courseId,
+                        exerciseId: params.exerciseId,
+                        pathwayId: params.pathwayId,
+                      })
+                    );
+                  }}
+                >
+                  Start Editing
+                </Button>
+                <Box sx={{ flexGrow: 1 }} />
+              </Toolbar>
+            </Container>
+          </Box>
+        </AppBar>
+      )}
       {successfulExerciseCompletion ? (
         <CompletionComponent
           setSuccessfulExerciseCompletion={setSuccessfulExerciseCompletion}
