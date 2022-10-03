8dc96a81bd71805812871ae05b3570065844e99c
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2022-10-03 18:18:39 +0530

    added empty fields when click on plus icon to add fields

--- src/components/PathwayExercise/ContentEdit/index.js
+++ src/components/PathwayExercise/ContentEdit/index.js
@@ -5,6 +5,7 @@ import { PATHS, interpolatePath, versionCode } from "../../../constant";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import useStyles from "../styles";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import {
   Container,
   TextField,
@@ -12,8 +13,94 @@ import {
   Box,
   Button,
   Grid,
+  Dialog,
+  List,
+  ListItem,
+  ListItemText,
   Typography,
+  IconButton,
 } from "@mui/material";
+
+function BoxComponent(props) {
+  // const [isShown, setIsShown] = useState(false);
+  return (
+    <Box
+      style={{ border: "1 px", background: props.isShown ? "white" : "" }}
+      onMouseEnter={() => props.setIsShown(true)}
+      onMouseLeave={() => props.setIsShown(false)}
+    >
+      {/* {props.isShown && (
+        <IconButton variant="solid">
+          <AddCircleOutlineIcon onClick={() => props.iconClick()} />
+        </IconButton>
+      )} */}
+      {props.children}
+    </Box>
+  );
+}
+
+// const showFunctionalities = (showModal) => {
+//   const dropDownList = [
+//     "Heading 1",
+//     "Heading 2",
+//     "Bulleted List",
+//     "Numbered List",
+//     "Image",
+//     "Video",
+//     "Code",
+//     "Table",
+//     "Quote",
+//     "Divider",
+//   ];
+//   return (
+//     <Dialog
+//       onClose={() => {
+//         setShowModal(!showModal);
+//       }}
+//       open={showModal}
+//     >
+//       {/* <DialogTitle>Set backup account</DialogTitle> */}
+//       <List sx={{ pt: 0 }}>
+//         {/* {emails.map((email) => ( */}
+//         <ListItem
+//           button
+//           // onClick={() => handleListItemClick(email)}
+//           // key={email}
+//         >
+//           <ListItemText primary="text 1" />
+//         </ListItem>
+//         <ListItem button>
+//           <ListItemText primary="text 2" />
+//         </ListItem>
+//         <ListItem button>
+//           <ListItemText primary="text 3" />
+//         </ListItem>
+//         <ListItem button>
+//           <ListItemText primary="text 4" />
+//         </ListItem>
+//       </List>
+//       <List>
+//         {/* {allComponents[showOption]
+//           ? Object.keys(allComponents[showOption]).map((e) => {
+//               return (
+//                 <ListItem button>
+//                   <ListItemText
+//                     primary={e}
+//                     onClick={() => {
+//                       // allComponents[showOption]
+//                       // handleAdd()
+//                       console.log(e);
+//                     }}
+//                   />
+//                 </ListItem>
+//               );
+//             })
+//           : ""} */}
+//         {/* {console.log(allComponents[showOption])} */}
+//       </List>
+//     </Dialog>
+//   );
+// };
 
 function ContentEdit() {
   const user = useSelector(({ User }) => User);
@@ -24,6 +111,121 @@ function ContentEdit() {
   const courseId = params.courseId;
   const exerciseId = params.exerciseId;
   const classes = useStyles();
+  const [showModal, setShowModal] = useState(false);
+  const [showOption, setShowOption] = useState();
+  const [isShown, setIsShown] = useState(false);
+  const [index, setIndex] = useState();
+
+  const dropDownList = {
+    "Heading 1": "AddHeader1",
+    "Heading 2": "AddHeader2",
+    "Bulleted List": "",
+    "Numbered List": "",
+    Image: "image",
+    Video: "youtube",
+    Code: "code",
+    // "Table",
+    // "Quote",
+    // "Divider",
+  };
+
+  console.log("course", course);
+
+  const allComponents = {
+    option: {
+      AddOption: {
+        id: 1,
+        value: "",
+      },
+    },
+    output: {
+      AddHeader: {
+        component: "header",
+        variant: 3,
+        value: "",
+      },
+      AddText: {
+        component: "text",
+        value: "",
+      },
+    },
+    header: {
+      AddHeader1: {
+        component: "header",
+        variant: 1,
+        value: "",
+      },
+      AddHeader2: {
+        component: "header",
+        variant: 2,
+        value: "",
+      },
+    },
+    image: {
+      AddImage: {
+        component: "image",
+        value: "",
+      },
+    },
+    youtube: {
+      AddYoutube: {
+        component: "youtube",
+        value: "",
+      },
+    },
+    code: {
+      AddCode: {
+        component: "code",
+        value: "",
+      },
+    },
+  };
+  const handleAdd = (index, ap, aap) => {
+    console.log(index, "handleAdd index");
+    console.log("ap", ap);
+    var temp = [...course];
+    if (ap === "options") {
+      const addOption = { ...allComponents.option.AddOption };
+      addOption.id = temp[index].value[temp[index].value.length - 1].id + 1;
+      temp[index].value.push(addOption);
+      setCourse(temp);
+    }
+    if (ap === "AddHeader1") {
+      const AddHeader = { ...allComponents.header.AddHeader1 };
+      console.log("temp before", temp);
+      temp.splice(index, 0, AddHeader);
+      console.log("temp after", temp);
+      setCourse(temp);
+    }
+    if (ap === "AddHeader2") {
+      const AddHeader = { ...allComponents.header.AddHeader2 };
+      console.log("temp before", temp);
+      temp.splice(index, 0, AddHeader);
+      console.log("temp after", temp);
+      setCourse(temp);
+    }
+    if (ap === "image") {
+      const AddHeader = { ...allComponents.image.AddImage };
+      console.log("temp before", temp);
+      temp.splice(index, 0, AddHeader);
+      console.log("temp after", temp);
+      setCourse(temp);
+    }
+    if (ap === "youtube") {
+      const AddHeader = { ...allComponents.youtube.AddYoutube };
+      console.log("temp before", temp);
+      temp.splice(index, 0, AddHeader);
+      console.log("temp after", temp);
+      setCourse(temp);
+    }
+    if (ap === "code") {
+      const AddHeader = { ...allComponents.code.AddCode };
+      console.log("temp before", temp);
+      temp.splice(index, 0, AddHeader);
+      console.log("temp after", temp);
+      setCourse(temp);
+    }
+  };
 
   console.log("params", params);
 
@@ -122,6 +324,22 @@ function ContentEdit() {
 
   console.log("course", course);
 
+  function myFunction() {
+    // Create an "li" node:
+    const node = document.createElement("input");
+    // const node = React.createElement("div", null, `Hello ${this.props.toWhat}`);
+    // return node;
+
+    // Create a text node:
+    // const textnode = document.createTextNode("Water");
+
+    // Append the text node to the "li" node:
+    // node.appendChild(textnode);
+
+    // Append the "li" node to the list:
+    document.getElementById("myList").appendChild(node);
+  }
+
   return (
     <Container maxWidth="sm" sx={{ mt: 5 }}>
       {courseType === "assessment" ? (
@@ -129,9 +347,23 @@ function ContentEdit() {
           {course &&
             course.map((e, index) => {
               if (e.component === "questionExpression") {
+                console.log(e.component, "e.value", e.value);
                 return (
-                  <Box>
-                    <Typography>Question</Typography>
+                  <BoxComponent
+                    setIsShown={setIsShown}
+                    isShown={isShown}
+                    iconClick={(e) => handleAdd(index, "assessment")}
+                  >
+                    <Typography>
+                      Question Options
+                      {isShown && (
+                        <IconButton variant="solid">
+                          <AddCircleOutlineIcon
+                            onClick={(e) => handleAdd(index, "assessment")}
+                          />
+                        </IconButton>
+                      )}
+                    </Typography>
                     {/* <TextField
                       id="outlined-basic"
                       // label="Question"
@@ -157,10 +389,17 @@ function ContentEdit() {
                         temp[index].value = e.target.value;
                         setCourse(temp);
                       }}
+                      // value={course[index].value}
+                      // onChange={(e) => {
+                      //   var temp = [...course];
+                      //   temp[index].value[optionIndex].value = e.target.value;
+                      //   setCourse(temp);
+                      // }}
                     />
-                  </Box>
+                  </BoxComponent>
                 );
               } else if (e.component === "questionCode") {
+                console.log(e.component, "e.value", e.value);
                 return (
                   <Box>
                     <Typography>Code</Typography>
@@ -181,9 +420,23 @@ function ContentEdit() {
                   </Box>
                 );
               } else if (e.component === "options") {
+                console.log(e.component, "e.value", e.value);
                 return (
-                  <Box>
-                    <Typography>Options</Typography>
+                  <BoxComponent
+                    setIsShown={setIsShown}
+                    isShown={isShown}
+                    iconClick={(e) => handleAdd(index, "options")}
+                  >
+                    <Typography>
+                      Options
+                      {isShown && (
+                        <IconButton variant="solid">
+                          <AddCircleOutlineIcon
+                            onClick={(e) => handleAdd(index, "options")}
+                          />
+                        </IconButton>
+                      )}
+                    </Typography>
                     {e.value.map((options, optionIndex) => {
                       return (
                         <TextField
@@ -203,9 +456,10 @@ function ContentEdit() {
                         />
                       );
                     })}
-                  </Box>
+                  </BoxComponent>
                 );
               } else if (e.component === "solution") {
+                console.log(e.component, "e.value", e.value);
                 return (
                   <>
                     <Typography>Solution</Typography>
@@ -226,6 +480,7 @@ function ContentEdit() {
                   </>
                 );
               } else if (e.component === "output") {
+                console.log(e.component, "e.value", e.value);
                 return Object.keys(course[index].value).map((sol, index1) => {
                   return (
                     <>
@@ -235,7 +490,7 @@ function ContentEdit() {
                         return (
                           <TextField
                             id="outlined-basic"
-                            label="Output"
+                            label={"Output " + solution.component}
                             variant="outlined"
                             fullWidth
                             // className={classes.editField}
@@ -266,8 +521,20 @@ function ContentEdit() {
             course.map((e, index) => {
               if (e.component === "header") {
                 return (
-                  <>
-                    <Typography>Header</Typography>
+                  <BoxComponent setIsShown={setIsShown} isShown={isShown}>
+                    <Typography>
+                      Header
+                      {isShown && (
+                        <IconButton variant="solid">
+                          <AddCircleOutlineIcon
+                            onClick={(e) => {
+                              setShowModal(!showModal);
+                              setIndex(index);
+                            }}
+                          />
+                        </IconButton>
+                      )}
+                    </Typography>
                     <TextField
                       id="outlined-basic"
                       label="Header"
@@ -277,16 +544,29 @@ function ContentEdit() {
                       value={course[index].value}
                       onChange={(e) => {
                         var temp = [...course];
-                        temp[index].value = e.target.value;
+                        temp[index].value[index].value = e.target.value;
                         setCourse(temp);
                       }}
                     />
-                  </>
+                    <div id="myList"></div>
+                  </BoxComponent>
                 );
               } else if (e.component === "code") {
                 return (
-                  <>
-                    <Typography>Code</Typography>
+                  <BoxComponent setIsShown={setIsShown} isShown={isShown}>
+                    <Typography>
+                      Code
+                      {isShown && (
+                        <IconButton variant="solid">
+                          <AddCircleOutlineIcon
+                            onClick={(e) => {
+                              setShowModal(!showModal);
+                              setIndex(index);
+                            }}
+                          />
+                        </IconButton>
+                      )}
+                    </Typography>
                     <TextField
                       id="outlined-basic"
                       label="Code"
@@ -300,12 +580,24 @@ function ContentEdit() {
                         setCourse(temp);
                       }}
                     />
-                  </>
+                  </BoxComponent>
                 );
               } else if (e.component === "text") {
                 return (
-                  <>
-                    <Typography>Text</Typography>
+                  <BoxComponent setIsShown={setIsShown} isShown={isShown}>
+                    <Typography>
+                      Text
+                      {isShown && (
+                        <IconButton variant="solid">
+                          <AddCircleOutlineIcon
+                            onClick={(e) => {
+                              setShowModal(!showModal);
+                              setIndex(index);
+                            }}
+                          />
+                        </IconButton>
+                      )}
+                    </Typography>
 
                     <TextField
                       id="outlined-basic"
@@ -320,13 +612,27 @@ function ContentEdit() {
                         setCourse(temp);
                       }}
                     />
-                  </>
+                  </BoxComponent>
                 );
               } else if (e.component === "youtube") {
                 return (
-                  <>
-                    <Typography>Youtube</Typography>
-
+                  <BoxComponent setIsShown={setIsShown} isShown={isShown}>
+                    <Typography>
+                      Youtube
+                      {/* <IconButton variant="solid">
+                        <AddCircleOutlineIcon />
+                      </IconButton> */}
+                      {isShown && (
+                        <IconButton variant="solid">
+                          <AddCircleOutlineIcon
+                            onClick={(e) => {
+                              setShowModal(!showModal);
+                              setIndex(index);
+                            }}
+                          />
+                        </IconButton>
+                      )}
+                    </Typography>
                     <TextField
                       id="outlined-basic"
                       label="Youtube"
@@ -340,13 +646,24 @@ function ContentEdit() {
                         setCourse(temp);
                       }}
                     />
-                  </>
+                  </BoxComponent>
                 );
               } else if (e.component === "image") {
                 return (
-                  <>
-                    <Typography>Image</Typography>
-
+                  <BoxComponent setIsShown={setIsShown} isShown={isShown}>
+                    <Typography>
+                      Image
+                      {isShown && (
+                        <IconButton variant="solid">
+                          <AddCircleOutlineIcon
+                            onClick={(e) => {
+                              setShowModal(!showModal);
+                              setIndex(index);
+                            }}
+                          />
+                        </IconButton>
+                      )}
+                    </Typography>
                     <TextField
                       id="outlined-basic"
                       label="Image"
@@ -360,7 +677,7 @@ function ContentEdit() {
                         setCourse(temp);
                       }}
                     />
-                  </>
+                  </BoxComponent>
                 );
               }
             })}
@@ -370,6 +687,97 @@ function ContentEdit() {
           </Button>
         </>
       )}
+
+      {console.log("showModal", showModal)}
+
+      {showModal && (
+        <Dialog
+          onClose={() => {
+            setShowModal(!showModal);
+          }}
+          open={showModal}
+        >
+          <List sx={{ pt: 0 }}>
+            {Object.keys(dropDownList).map((item) => (
+              <ListItem
+                button
+                onClick={() => {
+                  setShowModal(!showModal);
+                  handleAdd(index, dropDownList[item]);
+                  // dropDownList[item]
+                }}
+              >
+                <ListItemText primary={item} />
+              </ListItem>
+            ))}
+            {/* <ListItem
+              button
+              // onClick={() => handleListItemClick(email)}
+              // key={email}
+            >
+              <ListItemText primary="text 1" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="text 2" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="text 3" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="text 4" />
+            </ListItem> */}
+          </List>
+        </Dialog>
+      )}
+
+      {/* {showModal && (
+        <Dialog
+          onClose={() => {
+            setShowModal(!showModal);
+          }}
+          open={showModal}
+        >
+          <DialogTitle>Set backup account</DialogTitle>
+          <List sx={{ pt: 0 }}>
+            {emails.map((email) => (
+            <ListItem
+              button
+              // onClick={() => handleListItemClick(email)}
+              // key={email}
+            >
+              <ListItemText primary="text 1" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="text 2" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="text 3" />
+            </ListItem>
+            <ListItem button>
+              <ListItemText primary="text 4" />
+            </ListItem>
+          </List>
+          <List>
+            {allComponents[showOption]
+              ? Object.keys(allComponents[showOption]).map((e) => {
+                  return (
+                    <ListItem button>
+                      <ListItemText
+                        primary={e}
+                        onClick={() => {
+                          // allComponents[showOption]
+                          // handleAdd()
+                          console.log(e);
+                        }}
+                      />
+                    </ListItem>
+                  );
+                })
+              : ""}
+            {console.log(allComponents[showOption])}
+          </List>
+        </Dialog>
+      )} */}
     </Container>
   );
 }
