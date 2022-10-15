af2ea8ff5b23e6910b5c7071d378c3c6667228ca
jschanker <jschanker@molloy.edu>
2022-10-14 23:23:02 -0400

    Add margin prop to `DropdownLink`, fix `sx`

--- src/components/Header/DropdownLink.js
+++ src/components/Header/DropdownLink.js
@@ -8,6 +8,7 @@ export default function DropdownLink({
   to,
   external,
   padding,
+  margin,
   linkOnClick,
   children,
   ...props
@@ -19,16 +20,17 @@ export default function DropdownLink({
     <MenuItem
       {...props}
       sx={{
-        padding: 0
+        padding: 0,
+        margin: 0
       }}
     >
       <A
         {...{
           [toKey]: to,
           className: classes.link,
-          sx: { padding, display: 'inline-block' },
           linkOnClick
         }}
+        sx={{ padding, margin, display: 'inline-block' }},
       >
         {children}
       </A>
