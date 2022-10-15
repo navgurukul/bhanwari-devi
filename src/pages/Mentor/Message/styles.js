07ffc3c1deeeb75cc1e90e5de4ecea2cbf99d0c3
amansharmma <aman18@navgurukul.org>
2022-10-15 18:31:19 +0530

    A replaced button from the MUI button changed the color for the (join the class) & (Yes) buttons in the chat feat.

--- src/pages/Mentor/Message/styles.js
+++ src/pages/Mentor/Message/styles.js
@@ -8,6 +8,28 @@ const useStyles = makeStyles(
       color: "#2E2E2E",
       fontWeight: "400",
     },
+    labelButton: {
+      borderRadius: "4px",
+      display: "block",
+      alignSelf: "flex-start",
+      color: "#fff",
+      padding: "0 16px",
+      height: "60px",
+      fontWeight: "bold",
+      fontSize: "16px",
+      border: 0,
+      marginTop: "12px",
+      cursor: "pointer",
+      minWidth: "200px",
+      transition: "background-color 0.2s linear",
+      "& hover": {
+        backgroundColor: "#48A145",
+      },
+
+      "& self": {
+        alignSelf: "flex-start",
+      },
+    },
   }),
   { index: 1 }
 );
