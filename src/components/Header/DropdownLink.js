e4e5dbfc14decb76377ec94a4147c89f9e3d561b
jschanker <jschanker@molloy.edu>
2022-10-15 02:32:25 -0400

    Fix/Use camel case for `alignItems`

--- src/components/Header/DropdownLink.js
+++ src/components/Header/DropdownLink.js
@@ -30,7 +30,7 @@ export default function DropdownLink({
           className: classes.link,
           linkOnClick
         }}
-        style={{ padding, display: 'flex', align-items: 'center', width: '100%' }}
+        style={{ padding, display: 'flex', alignItems: 'center', width: '100%' }}
       >
         {children}
       </A>
