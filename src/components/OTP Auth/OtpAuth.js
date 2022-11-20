ac1968aad112e47ece3dba53c592ce4fb42f9154
pratikartik <kartikshikhare26@gmail.com>
2022-11-20 17:56:14 +0530

    Added verify OTP to PROFILE

---
+++ src/components/OTP Auth/OtpAuth.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OtpAuth() {
+  return (
+    <div>OtpAuth</div>
+  )
+}
+
+export default OtpAuth
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -25,28 +25,58 @@ import {
   Box,
   FormControl,
   FormHelperText,
+  Snackbar,
 } from "@mui/material";
 import { actions } from "../../components/User/redux/action";
 import useStyles from "./styles";
 import DropOutBatchesProfile from "../../components/DropOutBatches/DropOutBatchesProfile";
+import {
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
 
 function Profile() {
   const classes = useStyles();
   const user = useSelector(({ User }) => User);
   const [userData, setUserData] = useState([]);
-  const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(true);
   const [editName, setEditName] = useState();
   const [msg, setMsg] = useState();
   const [LoadBatches, setLoadBatches] = useState(false);
   const dispatch = useDispatch();
   const [helperText, setHelperText] = useState();
   const [showError, setShowError] = useState(false);
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(true);
   const [imgDialogs, setImgDialoags] = useState(false);
   const [imgCrop, setImgCrop] = useState(false);
   const [storeImg, setStoreImg] = useState([]);
   const isActive = useMediaQuery("(max-width:" + breakpoints.values.sm + "px)");
+  const [countryCode, setCountryCode] = useState(
+    `+${user?.data?.user?.contact?.split("-")?.[0]}` || "+91"
+  );
+  const [contact, setContact] = useState(
+    user?.data?.user?.contact?.split("-")?.[1]
+  );
+  const [confirmationResult, setConfirmationResult] = React.useState(null);
+  const [message, setMessage] = React.useState("");
+  const [startOtp, setStartOtp] = React.useState(false);
 
+  const [otp, setOtp] = React.useState("");
+  const [snackBarOpen, setSnackBarOpen] = React.useState(false);
+  const handleSnackBarClose = () => {
+    setSnackBarOpen(false);
+    setMessage("");
+  };
   const onCrop = (view) => {
     setImgCrop(view);
   };
@@ -70,10 +100,66 @@ function Profile() {
   const handleClose = () => {
     setOpen(false);
   };
-
+  // OTP AUTH FUNCTION
+  const app = initializeApp(firebaseConfig);
+  const onSignInSubmit = (event) => {
+    event.preventDefault();
+    if (!confirmationResult) {
+      setupRecaptcha();
+    }
+    const phoneNumber = `${countryCode}${contact}`;
+    const appVerifier = window.recaptchaVerifier;
+    const auth = getAuth();
+    console.log("phoneNumber", phoneNumber);
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+      .then((result) => {
+        setMessage("OTP sent successfully");
+        setSnackBarOpen(true);
+        console.log("OTP sent", result);
+        setStartOtp(true);
+        setConfirmationResult(result);
+      })
+      .catch((error) => {
+        console.log(error);
+        setMessage("Enter valid phone number");
+        setOpen(true);
+      });
+  };
+  const setupRecaptcha = () => {
+    const auth = getAuth();
+    window.recaptchaVerifier = new RecaptchaVerifier(
+      "recaptcha-container",
+      {
+        size: "invisible",
+        callback: (response) => {
+          onSignInSubmit();
+        },
+      },
+      auth
+    );
+  };
+  const OtpEnter = (event) => {
+    confirmationResult
+      .confirm(otp)
+      .then((result) => {
+        const user = result.user;
+        if (!user.isAnonymous) {
+          setShowError(false);
+          setMessage("Phone number verified successfully");
+          setSnackBarOpen(true);
+        }
+      })
+      .catch((error) => {
+        setMessage("enter valid otp");
+        setOpen(true);
+        setOtp("");
+      });
+  };
+  // OTP AUTH FUNCTION
   useEffect(() => {
     setEditName(user.data.user.name);
     setUserData(user.data.user);
+    console.log("user");
   }, []);
 
   useEffect(() => {
@@ -106,6 +192,7 @@ function Profile() {
       },
       data: {
         name: editName,
+        contact: `${countryCode}-${contact}`,
       },
     }).then((res) => {
       dispatch(actions.onUserRefreshDataIntent({ token: user.data.token }));
@@ -199,10 +286,11 @@ function Profile() {
                 registration progress intact in case you return back.
               </DialogContentText>
             </DialogContent> */}
+                <div id="recaptcha-container"></div>
                 <DialogActions>
                   <Box>
                     <TextField
-                      error={showError}
+                      error={editName?.length == 0}
                       // id="standard-basic"
                       label="Name"
                       fullWidth
@@ -227,37 +315,87 @@ function Profile() {
                     </Typography>
                   </Box>
                 </DialogActions>
-                {/* <Box my = {4} p = {1}>
-            <Grid container spacing={2} >
-          <Grid item xs={4}>
-            <MuiPhoneNumber
-              preferredCountries={["in"]}
-              defaultCountry={"in"}
-              variant="outlined"
-              id="countryCode"
-              // value={countryCode}
-              // onChange={(val) => {
-              //   setCountryCode(val);
-              //   return true;
-              // }}
-            />
-          </Grid>
-          <Grid item xs={8}>
-            <TextField
-              label="Phone Number"
-              // onChange={handleChange}
-              // value={contact}
-              name="contact"
-              id="contact"
-              variant="outlined"
-              // helperText="Enter Phone Number with Country Code"
-              fullWidth
-              // maxLength={12}
-            />
-          </Grid>
-        </Grid>
-        </Box> */}
-
+                <Box my={4} p={1}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                      <MuiPhoneNumber
+                        preferredCountries={["in"]}
+                        defaultCountry={"in"}
+                        variant="outlined"
+                        id="countryCode"
+                        value={countryCode}
+                        onChange={(val) => {
+                          setCountryCode(val);
+                        }}
+                      />
+                    </Grid>
+                    <Grid item xs={8}>
+                      <TextField
+                        label="Phone Number"
+                        onChange={(e) => {
+                          setContact(e.target.value);
+                        }}
+                        value={contact}
+                        name="contact"
+                        id="contact"
+                        variant="outlined"
+                        helperText="Enter Phone Number"
+                        fullWidth
+                        maxLength={10}
+                        error={contact?.length != 10 && contact?.length > 0}
+                      />
+                    </Grid>
+                    <Grid item xs={8}>
+                      <Button
+                        variant="outlined"
+                        onClick={(event) => {
+                          onSignInSubmit(event);
+                          setShowError(true);
+                        }}
+                        disabled={contact?.length != 10}
+                      >
+                        Get OTP
+                      </Button>{" "}
+                    </Grid>
+                    {startOtp && (
+                      <>
+                        <Grid item xs={8}>
+                          <TextField
+                            label="OTP"
+                            onChange={(e) => {
+                              setOtp(e.target.value);
+                            }}
+                            value={otp}
+                            name="OTP"
+                            id="contact"
+                            variant="outlined"
+                            helperText="Enter OTP"
+                            fullWidth
+                            maxLength={6}
+                            error={otp.length != 6 && otp.length > 0}
+                          />
+                        </Grid>
+                        <Grid item xs={8}>
+                          <Button
+                            variant="outlined"
+                            onClick={(e) => {
+                              OtpEnter(e);
+                            }}
+                            disabled={otp.length != 6}
+                          >
+                            Verify OTP
+                          </Button>{" "}
+                        </Grid>
+                      </>
+                    )}
+                  </Grid>
+                </Box>
+                <Snackbar
+                  anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                  open={snackBarOpen}
+                  onClose={handleSnackBarClose}
+                  message={message}
+                />
                 {/* {isEditing ? (
             <> */}
                 <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
