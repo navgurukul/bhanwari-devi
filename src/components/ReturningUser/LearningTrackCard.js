47e78ebc221cf12c850ab8a91a4bd391800916e4
saloni-080601 <salonipanwar532@gmail.com>
2022-11-30 16:43:28 +0530

    change in upcoming data

--- src/components/ReturningUser/LearningTrackCard.js
+++ src/components/ReturningUser/LearningTrackCard.js
@@ -160,7 +160,7 @@ function LearningTrackCard(props) {
   const pathwayCourseData = pathwayData.find((item) => {
     return item.id == pathwayId;
   });
-  console.log(pathwayCourseData);
+  // console.log(pathwayCourseData);
 
   return (
     <>
