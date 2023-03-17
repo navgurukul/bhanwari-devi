14a7997913c2282c3181f795039ef08845d46cf6
jschanker <jschanker@molloy.edu>
2023-03-17 08:19:32 -0700

    fix: Try redirecting to page not requiring authentication on logout

--- src/components/Header/UserMenu/index.js
+++ src/components/Header/UserMenu/index.js
@@ -95,7 +95,7 @@ function UserMenu() {
           </MenuItem>
         </NavLink>
         <Link
-          to={{}}
+          to={PATHS.OPPORTUNITIES}
           onClick={() => dispatch(userActions.logout())}
           className={classes.link}
         >
