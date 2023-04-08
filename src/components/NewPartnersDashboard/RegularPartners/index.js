1d0111f881e91dea80d9e779e16ec0842ef48c76
saloni-080601 <salonipanwar532@gmail.com>
2023-04-08 15:36:37 +0530

    ui of partner dashbpard

--- src/components/NewPartnersDashboard/RegularPartners/index.js
+++ src/components/NewPartnersDashboard/RegularPartners/index.js
@@ -6,6 +6,12 @@ import {
   TextField,
   InputAdornment,
   Grid,
+  Typography,
+  TableRow,
+  TableCell,
+  Chip,
+  Box,
+  TableHead,
 } from "@mui/material";
 import useStyles from "../style";
 import axios from "axios";
@@ -17,11 +23,28 @@ import EditIcon from "@mui/icons-material/Edit";
 import { Icon } from "@material-ui/core";
 import EmailIcon from "@mui/icons-material/Email";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CircleIcon from "@mui/icons-material/Circle";
+import { Pagination } from "@mui/material";
 
 function RegularPartnes() {
   const [volunteer, setVolunteer] = useState([]);
+  const [status, setStatus] = useState("");
+  const [page, setPage] = useState(0);
   const user = useSelector(({ User }) => User);
   const classes = useStyles();
+  const [currentPage, setCurrentPage] = useState(0);
+  const perPage = 10;
+  const pageCount = Math.ceil(volunteer.length / perPage);
+
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page - 1);
+  };
+
+  const displayedData = volunteer.slice(
+    currentPage * perPage,
+    (currentPage + 1) * perPage
+  );
 
   useEffect(() => {
     axios({
@@ -43,81 +66,144 @@ function RegularPartnes() {
     {
       name: "name",
       label: "Name",
+      options: {
+        sort: false,
+        filter: false,
+        fontSize: "600px",
+      },
     },
     {
       name: "pointofcontact",
       label: "Point of Contact",
+      options: {
+        sort: false,
+        filter: false,
+        customBodyRender: (value) => <TableCell>Arvind Shukla</TableCell>,
+      },
     },
     {
       name: "volunteer_id",
       label: "Number of Students",
+      options: {
+        sort: false,
+        filter: false,
+        customBodyRender: (value) => (
+          <div align="right">
+            <TableCell>{value === null ? "-" : value}</TableCell>
+          </div>
+        ),
+        customHeadLabelRender: (columnMeta) => (
+          <div style={{ textAlign: "right" }}>{columnMeta.label}</div>
+        ),
+      },
     },
     {
       name: "status",
       label: "Status",
-    },
-    {
-      name: "",
-      label: "",
       options: {
-        customBodyRender: (value) => (
-          <>
-            <Icon>
-              <EditIcon />
-            </Icon>
-            {value}
-          </>
-        ),
+        sort: false,
+        filter: false,
+        customBodyRender: (value) => {
+          return (
+            <div style={{ display: "flex" }}>
+              <CircleIcon
+                className={classes.circleIcon}
+                sx={{
+                  color: `${
+                    value === "active"
+                      ? "#48A145"
+                      : value === "inactive"
+                      ? "#FFCC00"
+                      : value === "dropout"
+                      ? "#F44336"
+                      : "#2196F3"
+                  }`,
+                }}
+              />
+              <TableCell sx={{ paddingLeft: "8px" }}>{value}</TableCell>
+            </div>
+          );
+        },
       },
     },
     {
-      name: "",
-      label: "",
+      name: "status",
+      label: " ",
       options: {
         customBodyRender: (value) => (
           <>
             <Icon>
-              <EmailIcon />
+              <EditIcon
+                color="grey"
+                sx={value === "active" && { "&:hover": { color: "#2196F3" } }}
+              />
             </Icon>
-            {value}
           </>
         ),
+
+        sort: false,
+        filter: false,
       },
     },
     {
-      name: "",
+      name: "status",
+      label: " ",
       options: {
         customBodyRender: (value) => (
           <>
             <Icon>
-              <DeleteIcon />
+              <EmailIcon
+                color="grey"
+                sx={value === "active" && { "&:hover": { color: "#48A145" } }}
+              />
             </Icon>
-            {value}
           </>
         ),
+        sort: false,
+        filter: false,
+      },
+    },
+    {
+      name: "status",
+      label: " ",
+      options: {
+        customBodyRender: (value) => (
+          // console.log(value)
+          <>
+            <Icon>
+              <DeleteIcon
+                color="grey"
+                sx={value === "active" && { "&:hover": { color: "#F44336" } }}
+              />
+            </Icon>
+          </>
+        ),
+        sort: false,
+        filter: false,
       },
     },
   ];
 
   const options = {
+    customToolbarSelect: () => {},
     filterType: "checkbox",
-    pagination: {
-      next: "Next Page",
-      previous: "Previous Page",
-      rowsPerPage: "Rows per page:",
-      displayRows: "of",
-    },
     responsive: "stacked",
+    pagination: false,
     filter: false,
     download: false,
     print: false,
     search: false,
     viewColumns: false,
+
     elevation: 0,
+    setRowProps: (rows, dataIndex, rowIndex) => {
+      return { sx: { backgroundColor: "transparent !important" } };
+    },
   };
+
   return (
-    <Container>
-      <Stack spacing={2} direction="row">
+    <Container sx={{ marginTop: "32px" }}>
+      <Stack spacing={2} direction="row" margin="16px 0px">
         <Button>Regular Partners</Button>
         <Button>State Level Partners</Button>
       </Stack>
@@ -137,7 +223,7 @@ function RegularPartnes() {
             fullWidth
           />
         </Grid>
-        <Grid item>
+        <Grid item md={2}>
           <Button
             variant="contained"
             sx={{ padding: "30px 16px", marginLeft: "20px" }}
@@ -146,13 +232,100 @@ function RegularPartnes() {
           </Button>
         </Grid>
       </Grid>
+      <Stack direction="row" spacing={1} margin="16px 0px">
+        <Chip
+          label="All Partners"
+          color="primary"
+          sx={{
+            fontSize: "14px",
+            fontWeight: "400",
+            fontFamily: "Noto Sans",
+            cursor: "pointer",
+          }}
+        />
+        <Chip
+          label="Newly Onboarded"
+          variant="outlined"
+          sx={{
+            fontSize: "14px",
+            fontWeight: "400",
+            fontFamily: "Noto Sans",
+            cursor: "pointer",
+          }}
+        />
+        <Chip
+          label="Active"
+          variant="outlined"
+          sx={{
+            fontSize: "14px",
+            fontWeight: "400",
+            fontFamily: "Noto Sans",
+            cursor: "pointer",
+          }}
+        />
+        <Chip
+          label="Inactive"
+          variant="outlined"
+          sx={{
+            fontSize: "14px",
+            fontWeight: "400",
+            fontFamily: "Noto Sans",
+            cursor: "pointer",
+          }}
+        />
+        <Chip
+          label="Archived"
+          variant="outlined"
+          sx={{
+            fontSize: "14px",
+            fontWeight: "400",
+            fontFamily: "Noto Sans",
+            cursor: "pointer",
+          }}
+        />
+      </Stack>
 
       <MUIDataTable
-        data={volunteer}
+        data={displayedData}
         columns={columns}
         options={options}
-        className={classes.tableBody}
+        className={classes.MuiTableRow}
       />
+      <div
+        style={{
+          position: "relative",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "32px",
+        }}
+      >
+        <Typography
+          variant="body2"
+          style={{ position: "absolute", left: 0, top: 0 }}
+        >
+          showing {currentPage * 10 + 1}- {(currentPage + 1) * 10} of{" "}
+          {volunteer.length}
+        </Typography>
+
+        <Pagination
+          style={{ margin: "0 auto" }}
+          count={pageCount}
+          page={currentPage + 1}
+          onChange={handlePageChange}
+          // variant="outlined"
+          // shape="circle"
+          classes={{ ul: "pagination" }}
+          color="primary"
+        />
+      </div>
+
+      <Stack
+        spacing={2}
+        sx={{
+          margin: "16px 0px",
+        }}
+      ></Stack>
     </Container>
   );
 }
--- src/components/NewPartnersDashboard/style.js
+++ src/components/NewPartnersDashboard/style.js
@@ -1,10 +1,34 @@
 import { makeStyles, createStyles } from "@mui/styles";
 
 const useStyles = makeStyles(() => ({
-  tableBody: {
+  MuiTableRow: {
     "& tbody": {
-      backgroundColor: "transparent",
+      "& tr": {
+        "& td": {
+          border: "none",
+        },
+      },
+      borderTop: "1px solid #BDBDBD",
+      borderBottom: "1px solid #BDBDBD",
     },
+    "& thead": {
+      "& tr": {
+        "& th": {
+          fontWeight: "600",
+          fontSize: "14px",
+        },
+      },
+    },
+  },
+  circleIcon: {
+    width: "8px !important",
+    height: "8px !important",
+    margin: "24px 0px 0px 0px",
+  },
+  ChipInput: {
+    fontSize: "14px",
+    fontWeight: "400",
+    fontFamily: "Noto Sans",
   },
 }));
 export default useStyles;
