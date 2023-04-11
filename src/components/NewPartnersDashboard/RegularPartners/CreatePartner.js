8a1d7a345fe0003323b8a4300e1c1958ccf955f0
gayatri <gayatrisanjayshelke20@navgurukul.org>
2023-04-11 23:13:46 +0530

    new changes

--- src/components/NewPartnersDashboard/RegularPartners/CreatePartner.js
+++ src/components/NewPartnersDashboard/RegularPartners/CreatePartner.js
@@ -27,59 +27,50 @@ function CreatePartner(props) {
     <div>
       <Dialog open={props.open} onClose={props.handleClose}>
         <DialogContent>
-            <Grid container mb={3}>
-              <Grid item xs={11}>
-                <Typography variant="h6" component="h2">
-                New partner
-                </Typography>
-              </Grid>
-              <Grid
-                color="text.secondary"
-                item
-                xs={1}
-
-                
-            >
-                <CloseIcon
-                  onClick={props.handleClose}
-                />
-              </Grid>
+          <Grid container mb={3}>
+            <Grid item xs={11}>
+              <Typography variant="h6" component="h2">
+                New Partner
+              </Typography>
             </Grid>
-         
+            <Grid color="text.secondary" item xs={1}>
+              <CloseIcon onClick={props.handleClose} />
+            </Grid>
+          </Grid>
           <TextField
             autoFocus
-            label="Partner Name"
-            name="name"
-            onChange={handleChange}
+            margin="dense"
+            label="Partner  Name"
             value={addpartner.name}
+            onChange={handleChange}
             fullWidth
           />
           <TextField
             autoFocus
+            margin="dense"
             label="Point Of Contact Name"
-            name="contact"
-            onChange={handleChange}
             value={addpartner.contact}
+            onChange={handleChange}
             fullWidth
           />
           <TextField
             autoFocus
-            label="Point Of Contact Email"
-            name="email"
-            onChange={handleChange}
+            margin="dense"
+            label="Point Of Contact Email "
             value={addpartner.email}
+            onChange={handleChange}
             fullWidth
           />
           <TextField
             autoFocus
+            margin="dense"
             label="Location (City/District)"
-            name="location"
-            onChange={handleChange}
             value={addpartner.location}
+            onChange={handleChange}
             fullWidth
           />
         </DialogContent>
-        <Box sx={{ p: 2, mt: -3 }}>
+        <Box sx={{ pb: 2, px: 2 }}>
           <DialogActions>
             <Button variant="contained" color="primary" fullWidth>
               Create Partner
