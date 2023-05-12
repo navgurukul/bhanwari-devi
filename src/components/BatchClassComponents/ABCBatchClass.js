177e6cd1d78cb77a8a1e7ef378b1c23a07265819
Komal Ahire <komala18@navgurukul.org>
2023-05-12 20:24:55 +0530

    create page for enroll classes batch

--- src/components/BatchClassComponents/ABCBatchClass.js
+++ src/components/BatchClassComponents/ABCBatchClass.js
@@ -1,4 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// import { METHODS } from "../../../services/api";
+import { METHODS } from "../../services/api";
+// import { PATHS } from "../../../constant";
+// import { format } from "../../../common/date";
+import { format } from "../../common/date";
+import { useSelector } from "react-redux";
 import {
   Box,
   Typography,
@@ -21,6 +28,24 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 function ABCBatchClass({ enrolledBatches }) {
   const isActive = useMediaQuery("(max-width:" + breakpoints.values.sm + "px)");
   const classes = useStyles();
+  const [enrollClasses, setEnrollClasses] = useState([]);
+  const user = useSelector(({ User }) => User);
+
+  useEffect(() => {
+    axios({
+      method: METHODS.GET,
+      url: `${process.env.REACT_APP_MERAKI_URL}/pathways/7/userEnrolledClasses`,
+      headers: {
+        accept: "application/json",
+        Authorization: user.data.token,
+      },
+    }).then((res) => {
+      setEnrollClasses(res.data);
+      // setPartner(response.data);
+      // console.log(res,'response')
+    });
+  }, []);
+
   return (
     <>
       <Grid
@@ -31,8 +56,7 @@ function ABCBatchClass({ enrolledBatches }) {
           isActive
             ? { padding: "32px 16px" }
             : { padding: "22px 80px 22px 80px" }
-        }
-      >
+        }>
         <Grid item>
           <Typography variant="h6" marginTop="8px">
             {enrolledBatches}
@@ -61,8 +85,7 @@ function ABCBatchClass({ enrolledBatches }) {
                 maxWidth: "384px",
                 background: "#FAFAFA",
               }}
-              elevation={1}
-            >
+              elevation={1}>
               <CardContent>
                 <Grid container>
                   <Grid item sm={2}>
@@ -102,6 +125,59 @@ function ABCBatchClass({ enrolledBatches }) {
           </Grid>
         </Grid>
 
+        <Container className={classes.cardGrid} maxWidth="lg">
+          <Grid container spacing={isActive ? 2 : 4}>
+            {enrollClasses.map((item) => (
+              <Grid item xs={12} ms={6} md={4}>
+                <Card
+                  sx={{
+                    padding: "32px 16px",
+                    maxWidth: "384px",
+                    background: "#FAFAFA",
+                  }}
+                  elevation={1}>
+                  <CardContent>
+                    <Grid container>
+                      <Grid item sm={2}>
+                        <img
+                          // className={classes.icons}
+                          src={require("./assets/playButton.svg")}
+                          alt="Students Img"
+                        />
+                      </Grid>
+                      <Grid item md={10} sm={8} xs={8}>
+                        <Typography marginLeft="16px" variant="subtitle1">
+                          {item.title}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                    <Grid container mt="32px">
+                      <Grid item md={7} xs={7} sx={{ display: "flex" }}>
+                        <DateRangeIcon />
+                        <Typography variant="body1" ml="8px">
+                          {format(item.start_time, "dd MMM yy")}
+                        </Typography>
+                      </Grid>
+                      <Grid display="flex">
+                        <AccessTimeIcon />
+                        <Typography variant="body1" marginLeft="8px">
+                          2 PM
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </CardContent>
+                  <CardActions sx={{ justifyContent: "right" }}>
+                    <Button disabled endIcon={<ArrowForwardIcon />}>
+                      Starts in 1 hour
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+
+        {/* 
         <Typography variant="h6" margin="32px 0px">
           Doubt Class
         </Typography>
@@ -112,8 +188,7 @@ function ABCBatchClass({ enrolledBatches }) {
                 padding: "32px 16px",
                 maxWidth: "384px",
                 background: "#FFF5CC",
-              }}
-            >
+              }}>
               <CardContent>
                 <Grid container>
                   <Grid item>
@@ -151,7 +226,7 @@ function ABCBatchClass({ enrolledBatches }) {
               </CardActions>
             </Card>
           </Grid>
-        </Grid>
+        </Grid> */}
       </Container>
     </>
   );
