ff570a620f1fa2457754c19981cd164365455bfa
Poonam-Singh-Bagh <poonambagh98@gmail.com>
2023-05-13 02:17:08 +0530

    Added enrolled functionality

--- src/components/BatchClassComponents/ABCBatchClass.js
+++ src/components/BatchClassComponents/ABCBatchClass.js
@@ -21,6 +21,7 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import useStyles from "./styles";
 import { breakpoints } from "../../theme/constant";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import DoubtClassCard from "./DoubtClassCard";
 
 function ABCBatchClass({ enrolledBatches }) {
   const isActive = useMediaQuery("(max-width:" + breakpoints.values.sm + "px)");
@@ -68,7 +69,8 @@ function ABCBatchClass({ enrolledBatches }) {
           isActive
             ? { padding: "32px 16px" }
             : { padding: "22px 80px 22px 80px" }
-        }>
+        }
+      >
         <Grid item>
           <Typography variant="h6" marginTop="8px">
             {enrolledBatches}
@@ -97,7 +99,8 @@ function ABCBatchClass({ enrolledBatches }) {
                 maxWidth: "384px",
                 background: "#FAFAFA",
               }}
-              elevation={1}>
+              elevation={1}
+            >
               <CardContent>
                 <Grid container>
                   <Grid item sm={2}>
@@ -160,7 +163,8 @@ function ABCBatchClass({ enrolledBatches }) {
                     background: "#FAFAFA",
                     background: index === 0 ? "#E9F5E9" : "#FAFAFA",
                   }}
-                  elevation={1}>
+                  elevation={1}
+                >
                   <CardContent>
                     <Grid container>
                       <Grid item sm={2}>
@@ -207,63 +211,15 @@ function ABCBatchClass({ enrolledBatches }) {
           </Grid>
         </Container>
 
-        <Typography variant="h6" margin="32px 0px">
-          Doubt Class
-        </Typography>
+        {doubtclasses && (
+          <Typography variant="h6" margin="32px 0px">
+            Doubt Class
+          </Typography>
+        )}
 
         <Grid container spacing={isActive ? 2 : 4}>
-          {doubtclasses.map((item, index) => (
-            <Grid item xs={12} ms={6} md={4}>
-              <Card
-                sx={{
-                  padding: "32px 16px",
-                  maxWidth: "384px",
-                  background: "#FFF5CC",
-                  // background: index === 0 ? "#E9F5E9" : "#FAFAFA",
-                }}
-                elevation={1}>
-                <CardContent>
-                  <Grid container>
-                    <Grid item sm={2}>
-                      <img
-                        // className={classes.icons}
-                        src={require("./assets/playButton.svg")}
-                        alt="Students Img"
-                      />
-                    </Grid>
-                    <Grid item md={10} sm={8} xs={8}>
-                      <Typography marginLeft="16px" variant="subtitle1">
-                        {item.title}
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                  <Grid container mt="32px">
-                    <Grid item md={7} xs={7} sx={{ display: "flex" }}>
-                      <DateRangeIcon />
-                      <Typography variant="body1" ml="8px">
-                        {format(item.start_time, "dd MMM yy")}
-                      </Typography>
-                    </Grid>
-                    <Grid display="flex">
-                      <AccessTimeIcon />
-                      <Typography variant="body1" marginLeft="8px">
-                        {/* 2 PM */}
-                        {format(item.start_time, "hh:mm aaa")}
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </CardContent>
-                <CardActions sx={{ justifyContent: "right" }}>
-                  <Button endIcon={<ArrowForwardIcon />}>
-                    <ClassJoinTimerButton
-                      buttonType="text"
-                      startTime={item.start_time}
-                      link={item.meet_link}
-                    />
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
+          {doubtclasses?.map((item, index) => (
+            <DoubtClassCard item={item} setDoubtclasses={setDoubtclasses} />
           ))}
         </Grid>
         {/* <Grid container>
