928ddac21b8d85a7991d71e52e97742dd5ffd05c
Komal Ahire <komala18@navgurukul.org>
2023-06-22 21:00:34 +0530

    done some changes

--- src/components/PathwayCourse/index.js
+++ src/components/PathwayCourse/index.js
@@ -319,6 +319,17 @@ function PathwayCourse() {
     setisFormModalOpen(true);
   };
 
+  // useEffect(() => {
+  //   if (!isFormFilled) {
+  //     console.log("komal---------")
+  //     window.scrollTo(0, 0);
+  //   }
+  // }, [isFormFilled]);
+
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <>
       <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
@@ -624,128 +635,87 @@ function PathwayCourse() {
               ) : null}
 
               <Grid container spacing={3} align="center">
-                {!isFormFilled && pathwayCode
-                  ? pathwayCourse?.data?.courses?.map((item, index) => (
-                      <Grid
-                        item
-                        key={index}
-                        xs={12}
-                        md={3}
-                        className={classes.courseCard}>
-                        <Card
-                          className={classes.pathwayCard}
-                          elevation={0}
+                {pathwayCourse?.data?.courses?.map((item, index) => (
+                  <Grid
+                    item
+                    key={index}
+                    xs={12}
+                    md={3}
+                    className={classes.courseCard}>
+                    <Link
+                      onClick={handleClick}
+                      className={classes.pathwayLink}
+                      to={
+                        !isFormFilled && pathwayCode
+                          ? "#"
+                          : interpolatePath(PATHS.PATHWAY_COURSE_CONTENT, {
+                              courseId: item.id,
+                              exerciseId: 0,
+                              pathwayId: pathwayId,
+                            })
+                      }>
+                      <Card
+                        className={classes.pathwayCard}
+                        elevation={0}
+                        onClick={onHandleSnackbarOpen}
+                        sx={{
+                          ml: 3,
+                          p: "16px",
+                          mb: isActive ? "0px" : "16px",
+                        }}>
+                        <img
+                          className={classes.courseImage}
+                          src={item.logo}
+                          alt="course"
+                        />
+                        <CardContent
                           sx={{
-                            ml: 3,
-                            p: "16px",
-                            mb: isActive ? "0px" : "16px",
-                          }}
-                          onClick={onHandleSnackbarOpen}>
-                          <img
-                            className={classes.courseImage}
-                            src={item.logo}
-                            alt="course"
-                          />
-                          <CardContent
-                            sx={{
-                              height: isActive ? "60px" : "70px",
-                              p: isActive ? "0px" : "0px 8px 0px 0px",
+                            height: isActive ? "60px" : "70px",
+                            p: isActive ? "0px" : "0px 8px 0px 0px",
+                            display: "flex",
+                          }}>
+                          {!isFormFilled && pathwayCode && (
+                            // <div>
+                              <LockIcon />
+                            // </div>
+                          )}
+                          <div className={classes.courseTitleNumber}>
+                            {/* {isFormFilled && ( */}
+                              <Typography
+                                align={isActive ? "center" : "left"}
+                                variant="body2"
+                                className={classes.courseName}
+                                sx={{
+                                  mr: "10px",
+                                  padding: isActive ? "5px" : "5px 0 5px 13px",
+                                  verticalAlign: "top",
+                                }}>
+                                {index + 1}
+                              </Typography>
+                            {/* )} */}
 
-                              display: "flex",
-                              alignItems: "center",
-                              gap: "0.6rem",
-                            }}>
-                            <LockIcon />
                             <Typography
                               align={isActive ? "center" : "left"}
                               variant="body1">
                               {item.name}
                             </Typography>
-                          </CardContent>
-                          <CardActions
-                            sx={{ height: "8px", padding: "8px 8px 8px 0px" }}>
-                            <LinearProgress
-                              className={classes.progressBar}
-                              variant="determinate"
-                              value={parseInt(completedPortion[item.id]) || 0}
-                            />
-                          </CardActions>
-                        </Card>
-                      </Grid>
-                    ))
-                  : pathwayCourse?.data?.courses?.map((item, index) => (
-                      <Grid
-                        item
-                        key={index}
-                        xs={12}
-                        md={3}
-                        className={classes.courseCard}>
-                        <Link
-                          className={classes.pathwayLink}
-                          to={interpolatePath(PATHS.PATHWAY_COURSE_CONTENT, {
-                            courseId: item.id,
-                            exerciseId: 0,
-                            pathwayId: pathwayId,
-                          })}>
-                          <Card
-                            className={classes.pathwayCard}
-                            elevation={0}
-                            sx={{
-                              ml: 3,
-                              p: "16px",
-                              mb: isActive ? "0px" : "16px",
-                            }}>
-                            <img
-                              className={classes.courseImage}
-                              src={item.logo}
-                              alt="course"
-                            />
-                            <CardContent
-                              sx={{
-                                height: isActive ? "60px" : "70px",
-                                p: isActive ? "0px" : "0px 8px 0px 0px",
-                              }}>
-                              <div className={classes.courseTitleNumber}>
-                                {!isFormFilled && pathwayCode ? (
-                                  <LockIcon />
-                                ) : (
-                                  <Typography
-                                    align={isActive ? "center" : "left"}
-                                    variant="body2"
-                                    className={classes.courseName}
-                                    sx={{
-                                      mr: "10px",
-                                      padding: isActive
-                                        ? "5px"
-                                        : "5px 0 5px 13px",
-                                      verticalAlign: "top",
-                                    }}>
-                                    {index + 1}
-                                  </Typography>
-                                )}
-
-                                <Typography
-                                  align={isActive ? "center" : "left"}
-                                  variant="body1">
-                                  {item.name}
-                                </Typography>
-                              </div>
-                            </CardContent>
-                            <CardActions
-                              sx={{
-                                height: "8px",
-                                padding: "8px 8px 8px 0px",
-                              }}>
-                              <LinearProgress
-                                className={classes.progressBar}
-                                variant="determinate"
-                                value={parseInt(completedPortion[item.id]) || 0}
-                              />
-                            </CardActions>
-                          </Card>
-                        </Link>
-                      </Grid>
-                    ))}
+                          </div>
+                        </CardContent>
+                        <CardActions
+                          sx={{
+                            height: "8px",
+                            padding: "8px 8px 8px 0px",
+                          }}>
+                          <LinearProgress
+                            className={classes.progressBar}
+                            variant="determinate"
+                            value={parseInt(completedPortion[item.id]) || 0}
+                          />
+                        </CardActions>
+                      </Card>
+                    </Link>
+                  </Grid>
+                ))}
               </Grid>
 
               {/* ...............certificate three dot button................ */}
