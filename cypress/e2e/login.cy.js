d70cf3750969e76e3e8d9e37b9cfaf40155e4682
jayshri15 <jayshri20@navgurukul.org>
2023-10-28 14:08:01 +0530

    UI Testing

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,103 +1,44 @@
 /// <reference types="cypress" />
 
-describe('Google auth', function () {
-    this.beforeAll(function () {
+describe('Meraki Learn Tests', function () {
+  this.beforeAll(function () {
+    // Log in with JWT token
+    cy.loginByJWT();
+  });
 
-    //   cy.task('db:seed')
-    // This is all about the UI testing with login meraki.
-    // All contain are here and all thing we coverd here like content and all
-    // Meraki login is done but using JWL token not a user id.
+  it('should navigate to a course and sections', function () {
+    // Visit the Meraki Learn website
+    cy.visit('https://www.merakilearn.org/');
 
-      cy.loginByJWT()
-    })
+    // Navigate to the "Introduction to Python" course
+    cy.get('.css-1t6c9ts > :nth-child(1)').contains('Learn').trigger('mouseover');
+    cy.get('.css-1bsvif8 > .MuiPaper-root > .MuiList-root > :nth-child(1)').contains('Introduction to Python').click();
 
-  // It all about Introduction to Python
-
-    it('shows onboarding', function () {
-      cy.visit('https://www.merakilearn.org/')
-      cy.get('.css-1t6c9ts > :nth-child(1)').contains('Learn').trigger('mouseover')
-      cy.get('.css-1bsvif8 > .MuiPaper-root > .MuiList-root > :nth-child(1)').contains('Introduction to Python').click()
-      cy.contains('Learn the basics and become comfortable in one of the most popular programming languages Python.')
-      cy.contains('Learning Outcomes')
-      cy.get(':nth-child(1) > .jss58 > .MuiPaper-root > .jss61').click()
-      cy.get(':nth-child(31) > .jss105 > .MuiListItemButton-root > .MuiTypography-root').click()
-      cy.get('.MuiButton-textPrimary').click()
-      cy.pause()
-
-      cy.get('.MuiBox-root > .MuiButton-root').click()
-
-      // cy.get('.MuiBox-root > .MuiButton-root').click()
-      // cy.get(':nth-child(2) > .jss157 > .MuiPaper-root > .MuiCardContent-root').click()
-      cy.contains('40. Practice Question').click()
-      // cy.get(':nth-child(41) > .jss148 > .MuiListItemButton-root > .MuiTypography-root')
-      // "#root > div > div > div:nth-child(2) > div > div > button"
-      cy.get('#root > div > div > div.MuiBox-root.css-0 > div > button.MuiButton-root.MuiButton-text.MuiButton-textPrimary.MuiButton-sizeMedium.MuiButton-textSizeMedium.MuiButtonBase-root.css-u2q5au').click()
-
-      cy.get('#root > div > div > div:nth-child(2) > div > div > button').click()
-
-      cy.pause()
-      cy.contains('41. Practice Question').click()
-      cy.get('#root > div > div > div.MuiBox-root.css-0 > div > button.MuiButton-root.MuiButton-text.MuiButton-textPrimary.MuiButton-sizeMedium.MuiButton-textSizeMedium.MuiButtonBase-root.css-u2q5au').click()
-
-      cy.get('#root > div > div > div:nth-child(2) > div > div > button').click()
-      cy.pause()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      // cy.get(':nth-child(2) > .jss105 > .MuiListItemButton-root').click()
-      // cy.get(':nth-child(3) > .jss105 > .MuiListItemButton-root > .MuiTypography-root').click()
-      // cy.get('.MuiButton-textPrimary').click()
-      // cy.get(':nth-child(27) > .jss105 > .MuiListItemButton-root > .MuiTypography-root').click()
-      // cy.contains('We can write comments in any program . When we run the program these comments do not get executed.')
-      // cy.get('[style="display: flex; justify-content: space-between; align-items: center;"] > .MuiTypography-root').click()
-      // cy.get(':nth-child(31) > .jss231 > .MuiListItemButton-root > .MuiTypography-root').click()
-
-
-      // It is all about Variable...
-
-      // cy.get('.css-1t6c9ts > :nth-child(1)').contains('Learn').trigger('mouseover')
-      // cy.get('.css-1bsvif8 > .MuiPaper-root > .MuiList-root > :nth-child(1)').contains('Introduction to Python').click()
-
-      // cy.get(':nth-child(2) > .jss284 > .MuiPaper-root').contains('Variables').click()
-
-
-
-
-
-      
-
-
-
-      
-      // cy.contains('Get Started').should('be.visible')
-    })
-
-    // it('shows onboarding', function () {
-    //   // cy.visit('https://www.merakilearn.org/')
-    //   // cy.get('.css-1t6c9ts > :nth-child(1)').contains('Learn').trigger('mouseover')
-
-    //   // cy.contains('Get Started').should('be.visible')
-    // })
-
-    this.afterAll(function () {
-      //   cy.task('db:seed')
-        cy.logOut()
-      })
-  })
+    // Click on specific sections ozr links you want to test
+    cy.contains('Introduction To Python').click()
+    cy.contains('how-to-begin').click();
+    cy.contains('Practice Question').click();
+    cy.contains('why').click();
+    cy.contains('30. Practice Question').click();
   
-  
+    cy.contains('Next').click();
+    cy.contains('Next Up: Variables').click();
+
+
+
+
+
+    // Add meaningful assertions to validate the behavior
+    // cy.contains('Some text you expect to see').should('be.visible');
+    // cy.url().should('include', '/expected-url');
+
+    // Add more interactions and assertions as needed
+  });
+
+  // Add more test cases as required
+
+  this.afterAll(function () {
+    // Log out or perform any necessary cleanup
+    cy.logOut();
+  });
+});
